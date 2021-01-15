97c6479482ca41e377cfeb7b590cbfa8a3241b96
iii <blbaileyiii.naa@gmail.com>
2021-01-14 17:11:38 -0800

    adding home page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,54 @@
-<?php
+<!DOCTYPE html>
+<html lang="en-us">
 
-  phpinfo();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CSE341 - Profile: Bernard Bailey</title>
+    <meta name="CSE341"
+        content="My Profile">
+    <link rel="stylesheet" href="../css/style.css">        
+</head>
 
-?>
+<body>
+    <main>        
+        <div class="console">
+            
+            <div class="border right"></div>
+            
+            <div class="border left"></div>
+            <div class="border top"></div>
+            <div class="border bottom"></div>            
+            <div class="content">            
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <br><br><br>
+                <div>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</div>
+                <div class="drive">Coming Soon</div>
+            </div>            
+        </div>
+    </main>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</html>
