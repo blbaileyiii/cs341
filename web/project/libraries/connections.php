e51c2ad8f35bf46600210874be476438f9660f44
iii <blbaileyiii.naa@gmail.com>
2021-02-01 12:15:44 -0800

    try again.

--- web/project/libraries/connections.php
+++ web/project/libraries/connections.php
@@ -4,28 +4,29 @@
 // <?php eowConnect();
 
 function eowConnect(){
-    $dbUrl = getenv('DATABASE_URL');
+    try {
+        $dbUrl = getenv('DATABASE_URL');
 
-    echo $dbUrl;
+        echo $dbUrl;
 
-    //$dbOpts = parse_url($dbUrl);
+        //$dbOpts = parse_url($dbUrl);
 
-    //$dbHost = $dbOpts["host"];
-    //$dbPort = $dbOpts["port"];
-    //$dbUser = $dbOpts["user"];
-    //$dbPassword = $dbOpts["pass"];
-    //$dbName = ltrim($dbOpts["path"],'/');
-    //$dsn = "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName";
-    //$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-    // Create the actual connection object and assign it to a variable
-    //try {
-    // $db = new PDO($dsn, $dbUser, $dbPassword, $options);
-    // echo "Working"
-    // return $db;
-    //} catch(PDOException $e) {
-    // echo 'Sorry, the connection failed';
+        //$dbHost = $dbOpts["host"];
+        //$dbPort = $dbOpts["port"];
+        //$dbUser = $dbOpts["user"];
+        //$dbPassword = $dbOpts["pass"];
+        //$dbName = ltrim($dbOpts["path"],'/');
+        //$dsn = "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName";
+        //$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+        // Create the actual connection object and assign it to a variable
+        
+        // $db = new PDO($dsn, $dbUser, $dbPassword, $options);
+        // echo "Working"
+        // return $db;
+    } catch(PDOException $e) {
+        echo 'Sorry, the connection failed';
     // header('Location: ./view/500.php');
     // exit;
-    //}
+    }
 }
 ?>
