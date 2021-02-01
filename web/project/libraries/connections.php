f901680d048c0155aba395a33d06e221aa88a98c
iii <blbaileyiii.naa@gmail.com>
2021-02-01 12:24:30 -0800

    next test

--- web/project/libraries/connections.php
+++ web/project/libraries/connections.php
@@ -9,26 +9,24 @@ function eowConnect(){
     try {
         $dbUrl = getenv('DATABASE_URL');
 
-        echo $dbUrl;
+        $dbOpts = parse_url($dbUrl);
 
-        //$dbOpts = parse_url($dbUrl);
-
-        //$dbHost = $dbOpts["host"];
-        //$dbPort = $dbOpts["port"];
-        //$dbUser = $dbOpts["user"];
-        //$dbPassword = $dbOpts["pass"];
-        //$dbName = ltrim($dbOpts["path"],'/');
-        //$dsn = "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName";
-        //$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+        $dsn = "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName";
+        $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
         // Create the actual connection object and assign it to a variable
         
-        // $db = new PDO($dsn, $dbUser, $dbPassword, $options);
-        // echo "Working"
-        // return $db;
+        $db = new PDO($dsn, $dbUser, $dbPassword, $options);
+        echo "Working"
+        return $db;
     } catch(PDOException $e) {
         echo 'Sorry, the connection failed';
-    // header('Location: ./view/500.php');
-    // exit;
+        header('Location: ./view/500.php');
+        exit;
     }
 }
 ?>
