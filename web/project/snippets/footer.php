b43998976566ce64565a694def27cf32b8cf5f11
iii <blbaileyiii.naa@gmail.com>
2021-02-01 11:22:16 -0800

    footer

--- web/project/snippets/footer.php
+++ web/project/snippets/footer.php
@@ -1,3 +1,9 @@
 <?php
-//NOT THE FOOTER TAG so it can be expanded...
+    <hr>
+    <p>
+        <?php 
+        $currYear = new DateTime('NOW');
+        echo '&copy;' . $currYear->format("Y") . ' | Bernard L. Bailey III | California | All rights reserved.' ?>
+        <br><?php echo "Last Updated: " . date("j F, Y", getlastmod());?>
+    </p>
 ?>
