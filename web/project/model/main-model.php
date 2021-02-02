15bd89e8002268b20657941ce47450a7c88a1a90
iii <blbaileyiii.naa@gmail.com>
2021-02-01 16:17:49 -0800

    test

--- web/project/model/main-model.php
+++ web/project/model/main-model.php
@@ -28,6 +28,7 @@ function getTaxonomy(){
    }
 
 function register(){
+        echo 'register function called';
     if(isset($_POST['register.email'])){
         echo "email is set...";
         $useremail = $_POST['register.email'];
