82a3f20fcd46a63cffdc1d3c274f477751d72d1f
iii <blbaileyiii.naa@gmail.com>
2021-02-02 09:22:31 -0800

    minor fix; testing again

--- web/project/snippets/header.php
+++ web/project/snippets/header.php
@@ -1,5 +1,5 @@
 <?php if ( isset($_SESSION['eowSession']['username']) ) { ?>
-    <span><a href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php $_SESSION["user"] ?>)</a></span>    
+    <span><a href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php $_SESSION['eowSession']['username'] ?>)</a></span>    
 <?php } else { ?>
     <span><a href="?action=getlogin" title="Echoes of Whimsy - Log In">Log In</a></span>
 <?php } ?>    
