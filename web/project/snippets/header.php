beb9c7bbff35c2ac8135d81e7b6748eeb1bdbc23
iii <blbaileyiii.naa@gmail.com>
2021-02-02 15:02:54 -0800

    adding logo

--- web/project/snippets/header.php
+++ web/project/snippets/header.php
@@ -1,3 +1,4 @@
+<span><a href="index.php" title="Echoes of Whimsy - News"><img src="/project/imgs/eow_logo.webp" alt="Echoes of Whimsy Logo"></a></span><span>Echoes of Whimsy</span>
 <?php if ( isset($_SESSION['eowSession']['username']) ) { ?>
     <span><a href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php echo $_SESSION['eowSession']['username'] ?>)</a></span>
     <span><a href="?action=logout" title="Echoes of Whimsy - Logout">Log out</a></span>   
