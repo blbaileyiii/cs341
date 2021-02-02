d97543d2898466e7e914a88ad7cb4384d9377177
iii <blbaileyiii.naa@gmail.com>
2021-02-02 15:25:38 -0800

    header layout

--- web/project/snippets/header.php
+++ web/project/snippets/header.php
@@ -1,7 +1,9 @@
 <span><a href="index.php" title="Echoes of Whimsy - News"><img src="/project/imgs/eow_logo.webp" alt="Echoes of Whimsy Logo"></a></span><span></span>
 <?php if ( isset($_SESSION['eowSession']['username']) ) { ?>
-    <span><a href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php echo $_SESSION['eowSession']['username'] ?>)</a></span>
-    <span><a href="?action=logout" title="Echoes of Whimsy - Logout">Log out</a></span>   
+    <div>
+        <span><a href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php echo $_SESSION['eowSession']['username'] ?>)</a></span>
+        <span><a href="?action=logout" title="Echoes of Whimsy - Logout">Log out</a></span>
+    </div>
 <?php } else { ?>
     <span><a href="?action=getlogin" title="Echoes of Whimsy - Log In">Log In</a></span>
 <?php } ?>    
