011558559367649b2d9f188c37e192f41b1e9a43
iii <blbaileyiii.naa@gmail.com>
2021-02-02 09:22:02 -0800

    testing session user

--- web/project/snippets/header.php
+++ web/project/snippets/header.php
@@ -1,4 +1,4 @@
-<?php if ( isset($_SESSION["user"]) ) { ?>
+<?php if ( isset($_SESSION['eowSession']['username']) ) { ?>
     <span><a href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php $_SESSION["user"] ?>)</a></span>    
 <?php } else { ?>
     <span><a href="?action=getlogin" title="Echoes of Whimsy - Log In">Log In</a></span>
