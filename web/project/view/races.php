efd68099bada7b6e1e7ce947d0bf2502a40f8cb7
iii <blbaileyiii.naa@gmail.com>
2021-02-02 09:58:30 -0800

    testing race dump

--- web/project/view/races.php
+++ web/project/view/races.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Echoes of Whimsy - Template</title>
+    <title>Echoes of Whimsy - Races and Creatures</title>
     <meta name="Echoes of Whimsy"
-        content="Echoes of Whimsy - Template">
+        content="Echoes of Whimsy - Races and Creatures">
         <link rel="stylesheet" href="/project/css/layout.css">
         <link rel="stylesheet" href="/project/css/typography.css">
         <link rel="stylesheet" href="/project/css/theme.css">
@@ -20,9 +20,10 @@
         <?php require $currRoot . '/project/snippets/nav.php'; ?>
     </nav>    
     <main>    
-        <h1>Content Title Here</h1>
-        Content goes here.
-        <?php var_dump($races); ?>
+        <h1>The Races and Creatures of Whimsy</h1>
+        <?php foreach ($races as $race) {?>
+            <? phpvar_dump($race); ?>
+        <?php } ?>
     </main>
     <footer>
     <?php require $currRoot . '/project/snippets/footer.php'; ?>
