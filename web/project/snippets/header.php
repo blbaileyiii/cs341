e7a1bba5f7ce00b8b7b120573a2bad819c41d3a3
iii <blbaileyiii.naa@gmail.com>
2021-02-03 07:11:29 -0800

    layout cont.

--- web/project/snippets/header.php
+++ web/project/snippets/header.php
@@ -1,13 +1,13 @@
 <div>
-        <span><a href="https://sites.google.com/view/toylifegames/home" title="Toy Life Games" target="_blank" rel="noreferrer">Toy Life Games</a></span>
+        <span><a class="header-lnk" href="https://sites.google.com/view/toylifegames/home" title="Toy Life Games" target="_blank" rel="noreferrer">Toy Life Games</a></span>
 </div>
 <?php if ( isset($_SESSION['eowSession']['username']) ) { ?>
     <div>
-        <span><a href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php echo $_SESSION['eowSession']['username'] ?>)</a></span>
-        <span><a href="?action=logout" title="Echoes of Whimsy - Logout">Log out</a></span>
+        <span><a class="header-lnk" href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php echo $_SESSION['eowSession']['username'] ?>)</a></span>
+        <span><a class="header-lnk" href="?action=logout" title="Echoes of Whimsy - Logout">Log out</a></span>
     </div>
 <?php } else { ?>
     <div>
-        <span><a href="?action=getlogin" title="Echoes of Whimsy - Log In">Log In</a></span>
+        <span><a class="header-lnk" href="?action=getlogin" title="Echoes of Whimsy - Log In">Log In</a></span>
     <div>
 <?php } ?>    
--- web/project/snippets/nav.php
+++ web/project/snippets/nav.php
@@ -1,7 +1,7 @@
 <nav class="nav">
-    <a class="nav-logo" href="index.php" title="Echoes of Whimsy - News"><img src="/project/imgs/eow_logo.webp" alt="Echoes of Whimsy Logo"></a>
+    <a class="nav-logo nav-lnk" href="index.php" title="Echoes of Whimsy - News"><img src="/project/imgs/eow_logo.webp" alt="Echoes of Whimsy Logo"></a>
     <ul class="nav-ul">
-        <li class="nav-li"><a href="/project/index.php" title="Echoes of Whimsy - News">News</a></li>
-        <li class="nav-li"><a href="/project/index.php?action=races" title="Echoes of Whimsy - Races and Creatures of Whimsy">The Races and Creatures of Whimsy</a></li>
+        <li class="nav-li"><a class="nav-lnk" href="/project/index.php" title="Echoes of Whimsy - News">News</a></li>
+        <li class="nav-li"><a class="nav-lnk" href="/project/index.php?action=races" title="Echoes of Whimsy - Races and Creatures of Whimsy">The Races and Creatures of Whimsy</a></li>
     </ul>
 </nav>
