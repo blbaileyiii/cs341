fc3d460b9416689fce10224ce4541a56ff32f044
iii <blbaileyiii.naa@gmail.com>
2021-02-02 18:18:40 -0800

    fix

--- web/project/snippets/header.php
+++ web/project/snippets/header.php
@@ -1,11 +1,13 @@
+<div>
+        <span><a href="https://sites.google.com/view/toylifegames/home" title="Toy Life Games" target="_blank" rel="noreferrer">Toy Life Games</a></span>
+</div>
 <?php if ( isset($_SESSION['eowSession']['username']) ) { ?>
-    <div>
-        <span><a href="https://sites.google.com/view/toylifegames/home" title="Toy Life Games LLC." target="_blank" rel="noreferrer">Toy Life Games</a></span>
-    </div>
     <div>
         <span><a href="?action=account" title="Echoes of Whimsy - My Account">My Account (<?php echo $_SESSION['eowSession']['username'] ?>)</a></span>
         <span><a href="?action=logout" title="Echoes of Whimsy - Logout">Log out</a></span>
     </div>
 <?php } else { ?>
-    <span><a href="?action=getlogin" title="Echoes of Whimsy - Log In">Log In</a></span>
+    <div>
+        <span><a href="?action=getlogin" title="Echoes of Whimsy - Log In">Log In</a></span>
+    <div>
 <?php } ?>    
