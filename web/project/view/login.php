4c0d50c00330a5c17bffa64943493cd4603238f0
iii <blbaileyiii.naa@gmail.com>
2021-02-05 14:26:57 -0800

    a

--- web/project/view/login.php
+++ web/project/view/login.php
@@ -19,8 +19,9 @@
     <?php require $currRoot. '/project/snippets/nav.php'; ?>  
     <main>
         <h1>Login</h1>
-            <div class="message"><?php echo $message ?></div>
+            
             <form class="form" method="post">
+                <div class="message"><?php echo $message ?></div>
                 <div class="fields">
                     <label for="uname"><span>User Name</span><span class="field-tip">Required</span></label>
                     <input id="uname" name="login[uname]" type="text" autocomplete="username" required>
