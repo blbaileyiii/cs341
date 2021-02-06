d0e3c5868098f91c47d09acdbb21edcb8eaf7860
iii <blbaileyiii.naa@gmail.com>
2021-02-06 09:51:55 -0800

    a

--- web/project/model/main-model.php
+++ web/project/model/main-model.php
@@ -346,7 +346,7 @@ function getCharactersHTML($characters) {
 
 function getCharacter($username, $userhashpass, $charname) {
     try {
-        $characters = [];
+        $character = [];
 
         $db = eowConnect();
 
@@ -362,19 +362,19 @@ function getCharacter($username, $userhashpass, $charname) {
 
         $stmt = $db->prepare($sql);
         $stmt->execute(array(':username' => $username, ':charname' => $charname));
-        $charactersSQL = $stmt->fetchAll();
+        $characterSQL = $stmt->fetchAll();
         //var_dump($charactersSQL);
 
-        foreach($charactersSQL as $characterSQL){
+        foreach($characterSQL as $charSQL){
 
-            $characters[$characterSQL['charname']]['charid'] = $characterSQL['charid'];
-            $characters[$characterSQL['charname']]['txracename'] = $characterSQL['txracename'];
-            $characters[$characterSQL['charname']]['txracedesc'] = $characterSQL['txracedesc'];
-            $characters[$characterSQL['charname']]['txfamilyname'] = $characterSQL['txfamilyname'];
-            $characters[$characterSQL['charname']]['txfamilydesc'] = $characterSQL['txfamilydesc'];
-            $characters[$characterSQL['charname']]['txgenusname'] = $characterSQL['txgenusname'];
-            $characters[$characterSQL['charname']]['txgenuspron'] = $characterSQL['txgenuspron'];
-            $characters[$characterSQL['charname']]['txgenusdesc'] = $characterSQL['txgenusdesc'];                
+            $character[$charSQL['charname']]['charid'] = $charSQL['charid'];
+            $character[$charSQL['charname']]['txracename'] = $charSQL['txracename'];
+            $character[$charSQL['charname']]['txracedesc'] = $charSQL['txracedesc'];
+            $character[$charSQL['charname']]['txfamilyname'] = $charSQL['txfamilyname'];
+            $character[$charSQL['charname']]['txfamilydesc'] = $charSQL['txfamilydesc'];
+            $character[$charSQL['charname']]['txgenusname'] = $charSQL['txgenusname'];
+            $character[$charSQL['charname']]['txgenuspron'] = $charSQL['txgenuspron'];
+            $character[$charSQL['charname']]['txgenusdesc'] = $charSQL['txgenusdesc'];                
         }
 
         // SELECT the character attributes from the corresponding characters.
@@ -388,15 +388,15 @@ function getCharacter($username, $userhashpass, $charname) {
 
         $stmt = $db->prepare($sql);
         $stmt->execute(array(':username' => $username));
-        $charsattribsSQL = $stmt->fetchAll();
+        $characterattribsSQL = $stmt->fetchAll();
         //var_dump($charattribsSQL);
 
-        foreach($charsattribsSQL as $charattribsSQL){
+        foreach($characterattribsSQL as $charattribsSQL){
 
-            $characters[$charattribsSQL['charname']]['attributes'][$charattribsSQL['attribname']]['attribabbrv'] = $charattribsSQL['attribabbrv'];
-            $characters[$charattribsSQL['charname']]['attributes'][$charattribsSQL['attribname']]['attribdesc'] = $charattribsSQL['attribdesc'];
-            $characters[$charattribsSQL['charname']]['attributes'][$charattribsSQL['attribname']]['attribtypeof'] = $charattribsSQL['attribtypeof'];
-            $characters[$charattribsSQL['charname']]['attributes'][$charattribsSQL['attribname']]['charattribval'] = $charattribsSQL['charattribval'];
+            $character[$charattribsSQL['charname']]['attributes'][$charattribsSQL['attribname']]['attribabbrv'] = $charattribsSQL['attribabbrv'];
+            $character[$charattribsSQL['charname']]['attributes'][$charattribsSQL['attribname']]['attribdesc'] = $charattribsSQL['attribdesc'];
+            $character[$charattribsSQL['charname']]['attributes'][$charattribsSQL['attribname']]['attribtypeof'] = $charattribsSQL['attribtypeof'];
+            $character[$charattribsSQL['charname']]['attributes'][$charattribsSQL['attribname']]['charattribval'] = $charattribsSQL['charattribval'];
         }
 
         // SELECT the character inventory from the corresponding characters.
@@ -410,15 +410,15 @@ function getCharacter($username, $userhashpass, $charname) {
 
         $stmt = $db->prepare($sql);
         $stmt->execute(array(':username' => $username));
-        $charsinvSQL = $stmt->fetchAll();
+        $characterinvSQL = $stmt->fetchAll();
         //var_dump($charinvSQL);
 
-        foreach($charsinvSQL as $charinvSQL){
+        foreach($characterinvSQL as $charinvSQL){
 
-            $characters[$charinvSQL['charname']]['inventory'][$charinvSQL['charinvslot']]['itemname'] = $charinvSQL['itemname'];
-            $characters[$charinvSQL['charname']]['inventory'][$charinvSQL['charinvslot']]['itemdescshort'] = $charinvSQL['itemdescshort'];
-            $characters[$charinvSQL['charname']]['inventory'][$charinvSQL['charinvslot']]['itemdesclong'] = $charinvSQL['itemdesclong'];
-            $characters[$charinvSQL['charname']]['inventory'][$charinvSQL['charinvslot']]['charinvqty'] = $charinvSQL['charinvqty'];
+            $character[$charinvSQL['charname']]['inventory'][$charinvSQL['charinvslot']]['itemname'] = $charinvSQL['itemname'];
+            $character[$charinvSQL['charname']]['inventory'][$charinvSQL['charinvslot']]['itemdescshort'] = $charinvSQL['itemdescshort'];
+            $character[$charinvSQL['charname']]['inventory'][$charinvSQL['charinvslot']]['itemdesclong'] = $charinvSQL['itemdesclong'];
+            $character[$charinvSQL['charname']]['inventory'][$charinvSQL['charinvslot']]['charinvqty'] = $charinvSQL['charinvqty'];
         }
 
         // SELECT the character skills from the corresponding characters.
@@ -433,14 +433,14 @@ function getCharacter($username, $userhashpass, $charname) {
 
         $stmt = $db->prepare($sql);
         $stmt->execute(array(':username' => $username));
-        $charsskillsSQL = $stmt->fetchAll();
+        $characterskillsSQL = $stmt->fetchAll();
         //var_dump($charskillsSQL);
 
-        foreach($charsskillsSQL as $charskillsSQL){
+        foreach($characterskillsSQL as $charskillsSQL){
 
-            $characters[$charskillsSQL['charname']]['skills'][$charskillsSQL['skillname']]['skilldescshort'] = $charskillsSQL['skilldescshort'];
-            $characters[$charskillsSQL['charname']]['skills'][$charskillsSQL['skillname']]['skilldesclong'] = $charskillsSQL['skilldesclong'];
-            $characters[$charskillsSQL['charname']]['skills'][$charskillsSQL['skillname']]['charskillxp'] = $charskillsSQL['charskillxp'];
+            $character[$charskillsSQL['charname']]['skills'][$charskillsSQL['skillname']]['skilldescshort'] = $charskillsSQL['skilldescshort'];
+            $character[$charskillsSQL['charname']]['skills'][$charskillsSQL['skillname']]['skilldesclong'] = $charskillsSQL['skilldesclong'];
+            $character[$charskillsSQL['charname']]['skills'][$charskillsSQL['skillname']]['charskillxp'] = $charskillsSQL['charskillxp'];
         }
 
         // The next line closes the interaction with the database 
@@ -448,72 +448,72 @@ function getCharacter($username, $userhashpass, $charname) {
         
         //var_dump($characters);
 
-        return $characters;
+        return $character;
 
     } catch(PDOException $ex) {
         echo $sql . "<br>" . $ex->getMessage();
     }
 }
 
-function getCharacterHTML($characters) {
+function getCharacterHTML($character) {
 
-    $charactersHTML = "";
+    $characterHTML = "";
 
-    if(count($characters) > 0) {
-        $charactersHTML .= "<section class='characters'>";
-        $charactersHTML .= "<h2>Characters</h2>";
-        foreach ($characters as $character => $characterInfo) {
-            $charactersHTML .= "<div>";
-            $charactersHTML .= "<h3>$character</h3>";
-            $charactersHTML .= "<div class='character-profile'>";
-            $charactersHTML .= "Race: <i>$characterInfo[txfamilyname] " . strtolower($characterInfo['txgenusname']) . "</i>";
-            $charactersHTML .= "</div>";
-            $charactersHTML .= "<div class='image'>";
-            $charactersHTML .= "</div>";
-            $charactersHTML .= "<section class='character-attributes'>";
-            $charactersHTML .= "<h4>Attributes</h4>";
-            $charactersHTML .= "<ul>";
+    if(count($character) > 0) {
+        $characterHTML .= "<section class='characters'>";
+        $characterHTML .= "<h2>Characters</h2>";
+        foreach ($character as $charname => $characterInfo) {
+            $characterHTML .= "<div>";
+            $characterHTML .= "<h3>$charname</h3>";
+            $characterHTML .= "<div class='character-profile'>";
+            $characterHTML .= "Race: <i>$characterInfo[txfamilyname] " . strtolower($characterInfo['txgenusname']) . "</i>";
+            $characterHTML .= "</div>";
+            $characterHTML .= "<div class='image'>";
+            $characterHTML .= "</div>";
+            $characterHTML .= "<section class='character-attributes'>";
+            $characterHTML .= "<h4>Attributes</h4>";
+            $characterHTML .= "<ul>";
             foreach($characterInfo['attributes'] as $attribute => $attributeInfo){
-                $charactersHTML .= "<li><span class='info-name'>$attributeInfo[attribabbrv]</span>: $attributeInfo[charattribval]</li>";
+                $characterHTML .= "<li><span class='info-name'>$attributeInfo[attribabbrv]</span>: $attributeInfo[charattribval]</li>";
             }
-            $charactersHTML .= "</ul>";
-            $charactersHTML .= "</section>";
-            $charactersHTML .= "<section class='character-skills'>";
-            $charactersHTML .= "<h4>Skills</h4>";
-            $charactersHTML .= "<ul>";
+            $characterHTML .= "</ul>";
+            $characterHTML .= "</section>";
+            $characterHTML .= "<section class='character-skills'>";
+            $characterHTML .= "<h4>Skills</h4>";
+            $characterHTML .= "<ul>";
             foreach($characterInfo['skills'] as $skill => $skillInfo){
-                $charactersHTML .= "<li>";
-                $charactersHTML .= "<span class='info-name'>$skill</span>";
+                $characterHTML .= "<li>";
+                $characterHTML .= "<span class='info-name'>$skill</span>";
                 //$charactersHTML .= "<ul>";
                 //$charactersHTML .= "<li>Description: $skillInfo[skilldescshort]</li>";
                 //$charactersHTML .= "<li>Additional: $skillInfo[skilldesclong]</li>";
                 //$charactersHTML .= "<li>Advancement: $skillInfo[charskillxp]</li>";
                 //$charactersHTML .= "</ul>";
-                $charactersHTML .= "</li>";
+                $characterHTML .= "</li>";
             }
-            $charactersHTML .= "</ul>";
-            $charactersHTML .= "</section>";
-            $charactersHTML .= "<section class='character-inventory'>";
-            $charactersHTML .= "<h4>Inventory</h4>";
-            $charactersHTML .= "<ul>";
+            $characterHTML .= "</ul>";
+            $characterHTML .= "</section>";
+            $characterHTML .= "<section class='character-inventory'>";
+            $characterHTML .= "<h4>Inventory</h4>";
+            $characterHTML .= "<ul>";
             foreach($characterInfo['inventory'] as $slot => $item){
-                $charactersHTML .= "<li>$item[itemname]: $item[charinvqty]</li>";
+                $characterHTML .= "<li>$item[itemname]: $item[charinvqty]</li>";
             }
-            $charactersHTML .= "</ul>";
-            $charactersHTML .= "</section>";
-            $charactersHTML .= "<div class='character-management'>";
-            $charactersHTML .= "<form method='post'>";
-            $charactersHTML .= "<input type='hidden' name='action' value='manage'>";
-            $charactersHTML .= "<button type='submit' name='edit' value='$characterInfo[charid]'>Edit</button>";
-            $charactersHTML .= "<button type='submit' name='delete' value='$characterInfo[charid]'>Delete</button>";
-            $charactersHTML .= "</form>";
-            $charactersHTML .= "</div>";
-            $charactersHTML .= "</div>";
+            $characterHTML .= "</ul>";
+            $characterHTML .= "</section>";
+            $characterHTML .= "<div class='character-management'>";
+            $characterHTML .= "<form method='post'>";
+            $characterHTML .= "<input type='hidden' name='action' value='manage'>";
+            $characterHTML .= "<button type='submit' name='edit' value='$characterInfo[charid]'>Edit</button>";
+            $characterHTML .= "<button type='submit' name='delete' value='$characterInfo[charid]'>Delete</button>";
+            $characterHTML .= "</form>";
+            $characterHTML .= "</div>";
+            $characterHTML .= "</div>";
         }
-        $charactersHTML .= "</section>";
+        $characterHTML .= "</section>";
     }
 
-    return $charactersHTML;
+    return $characterHTML;
 }
 
 ?>
