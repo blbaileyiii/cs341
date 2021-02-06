2672764beed948ad4691f0777c5875f246937fe8
iii <blbaileyiii.naa@gmail.com>
2021-02-06 09:00:33 -0800

    a

--- web/project/js/character.js
+++ web/project/js/character.js
@@ -3,6 +3,7 @@ const characters = document.querySelectorAll('.character');
 characters.forEach(character => character.addEventListener("click", getCharacter));
 
 function getCharacter(elem){
+    console.log(elem.target);
     let charid = elem.target.getAttribute(characterid);
-    alert(charid);
+    console.log(charid);
 }
