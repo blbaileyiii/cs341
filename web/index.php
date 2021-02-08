17d5e01fe7819d986dbdf868eb0ea0bd6b5ef4f6
iii <blbaileyiii.naa@gmail.com>
2021-02-08 11:44:26 -0800

    a

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@
     <title>CSE341 - Profile: Bernard Bailey</title>
     <meta name="CSE341"
         content="My Profile">
-    <link rel="stylesheet" href="css/style.css">        
+    <link rel="stylesheet" href="css/style.css">       
 </head>
 <body>            
     <div class="console">            
