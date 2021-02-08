7769cc6c5ee0a7ee15824546da8384657cd138f0
iii <blbaileyiii.naa@gmail.com>
2021-02-08 08:05:27 -0800

    a

---
+++ web/project/view/character-new.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Echoes of Whimsy - Character Creation</title>
+    <meta name="Echoes of Whimsy"
+        content="Echoes of Whimsy - Character Creation">
+        <link rel="stylesheet" href="/project/css/theme.css">
+        <link rel="stylesheet" href="/project/css/typography.css">
+        <link rel="stylesheet" href="/project/css/layout.css">
+</head>
+
+<body>
+    <header>
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/project/snippets/header.php'; ?>
+    </header>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/project/snippets/nav.php'; ?>   
+    <main>    
+        <h1>Character Creation</h1>
+        Content goes here.
+    </main>
+    <footer>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/project/snippets/footer.php'; ?>
+    </footer>
+</body>
+</html>
