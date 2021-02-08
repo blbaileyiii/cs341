0a5dc512962dea1d3396a9ed4fe80292442d70af
iii <blbaileyiii.naa@gmail.com>
2021-02-08 12:34:09 -0800

    a

--- web/project/view/character-create.php
+++ web/project/view/character-create.php
@@ -15,31 +15,51 @@
         <h1><?php echo $page ?></h1>
         <form>
             <div class="fields">
-                <label for=''><span></span><span class="field-tip">Required</span></label>
-                <input id='' name='' type="text" required>
+                <label for='charname'><span>Character Name</span><span class="field-tip">Required</span></label>
+                <input id='charname' name='charname' type="text" required>
             </div>
             <div class="fields">
-                <label for=''><span></span><span class="field-tip">Required</span></label>
-                <select value="" required>
+                <label for='txgenusid'><span></span><span class="field-tip">Required</span></label>
+                <select id='txgenusid' name='txgenusid' value="" required>
                     <option selected disabled>Choose a Race</option>
                     <?php echo $playableOptions ?>
                 </select>
             </div>
             <div class="fields">
-                <label for=''><span></span><span class="field-tip">Required</span></label>
-                <input id='' name='' type="text" required>
+                <label for='STR'><span></span><span class="field-tip">Required</span></label>
+                <input id='STR' name='STR' type="number" step="1" min="1" placeholder="1" required>
             </div>
             <div class="fields">
-                <label for=''><span></span><span class="field-tip">Required</span></label>
-                <input id='' name='' type="text" required>
+                <label for='DEX'><span></span><span class="field-tip">Required</span></label>
+                <input id='DEX' name='DEX' type="number" step="1" min="1" placeholder="1" required>
             </div>
             <div class="fields">
-                <label for=''><span></span><span class="field-tip">Required</span></label>
-                <input id='' name='' type="text" required>
+                <label for='AGL'><span></span><span class="field-tip">Required</span></label>
+                <input id='AGL' name='AGL' type="number" step="1" min="1" placeholder="1" required>
             </div>
             <div class="fields">
-                <label for=''><span></span><span class="field-tip">Required</span></label>
-                <input id='' name='' type="text" required>
+                <label for='END'><span></span><span class="field-tip">Required</span></label>
+                <input id='END' name='END' type="number" step="1" min="1" placeholder="1" required>
+            </div>
+            <div class="fields">
+                <label for='INT'><span></span><span class="field-tip">Required</span></label>
+                <input id='INT' name='INT' type="number" step="1" min="1" placeholder="1" required>
+            </div>
+            <div class="fields">
+                <label for='ATH'><span></span><span class="field-tip">Required</span></label>
+                <input id='ATH' name='ATH' type="number" step="1" min="1" placeholder="1" required>
+            </div>
+            <div class="fields">
+                <label for='PER'><span></span><span class="field-tip">Required</span></label>
+                <input id='PER' name='PER' type="number" step="1" min="1" placeholder="1" required>
+            </div>
+            <div class="fields">
+                <label for='LCK'><span></span><span class="field-tip">Required</span></label>
+                <input id='LCK' name='LCK' type="number" step="1" min="1" placeholder="1" required>
+            </div>
+            <div class="fields">
+                <label for='CHA'><span></span><span class="field-tip">Required</span></label>
+                <input id='CHA' name='CHA' type="number" step="1" min="1" placeholder="1" required>
             </div>
         </form>
     </main>
