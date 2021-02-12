c2d53b78b2045636acac8ca86ede74f044070c7d
iii <blbaileyiii.naa@gmail.com>
2021-02-12 13:30:40 -0800

    a

--- web/project/model/char-mgmt-model.php
+++ web/project/model/char-mgmt-model.php
@@ -207,7 +207,7 @@ function getCharacterHTML($character) {
             $characterHTML .= "<h2>Attributes</h2>";
             $characterHTML .= "<ul>";
             foreach($characterInfo['attributes'] as $attribute => $attributeInfo){
-                $characterHTML .= "<li><span class='info-name'>$attribute</span>: $attributeInfo[charattribval]</li>";
+                $characterHTML .= "<li><span class='info-name'>$attributeInfo[attribabbrv]</span>: $attributeInfo[charattribval]</li>";
             }
             $characterHTML .= "</ul>";
             $characterHTML .= "</section>";
@@ -277,7 +277,7 @@ function getCharEditHTML($character, $playableOptions) {
             $characterHTML .= "<h2>Attributes</h2>";
             foreach($characterInfo['attributes'] as $attribute => $attributeInfo){
                 $characterHTML .= "<div class='fields'>";
-                $characterHTML .= "<label for=''><span class='info-name'>$attributeInfo[attribname]</span><span class='field-tip'>Required</span></label>";
+                $characterHTML .= "<label for=''><span class='info-name'>$attribute</span><span class='field-tip'>Required</span></label>";
                 $characterHTML .= "<input type='number' value='$attributeInfo[charattribval]' min='$attributeInfo[charattribval]' step='1'>";
                 $characterHTML .= "</div>";
             }
