be0083d40bef9b9e73f5828b68f45bb6c7367b35
iii <blbaileyiii.naa@gmail.com>
2021-02-12 19:40:44 -0800

    troubleshooting

--- web/project/model/char-mgmt-model.php
+++ web/project/model/char-mgmt-model.php
@@ -444,7 +444,7 @@ function saveEdits($username, $character){
                 }
             }
         } catch(PDOException $ex) {
-            echo $sql . "<br>" . $ex->getMessage();
+            echo "<br><br>" . $sql . "<br>" . $ex->getMessage() . "<br><br>" . $tokens;
         }
 
     }
