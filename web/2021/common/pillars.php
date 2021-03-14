3c500b77567151ae53476516928ac57a93005de4
iii <blbaileyiii.naa@gmail.com>
2021-03-14 10:51:12 -0700

    a

--- web/2021/common/pillars.php
+++ web/2021/common/pillars.php
@@ -1,9 +1,10 @@
 <a class="anchor" id="focus"></a>
 <section>
     <h2>The Focus</h2>
-    <img src='/2021/images/spiritual_social_intellectual_physical_cyd_logo.png' alt=''>
+    <img src='/2021/images/spiritual_social_intellectual_physical_cyd_logo.webp' alt='Church of Jesus Christ of Latter-Day Saints Youth Pillars'>
     <div>
         <h3>Spiritual</h3>
+        <img src='/2021/images/spiritual-w1280x854.webp' alt='Reading the Scriptures'>
         <p>Content Here</p>
     </div>
     <div>
