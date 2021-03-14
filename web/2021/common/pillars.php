2903c9df748f7d52b9228c900fbd3e6c6c15bdf5
iii <blbaileyiii.naa@gmail.com>
2021-03-14 16:06:07 -0700

    a

--- web/2021/common/pillars.php
+++ web/2021/common/pillars.php
@@ -5,21 +5,21 @@
     <div>
         <h3>Spiritual</h3>
         <img src='/2021/images/spiritual-w1280x854.webp' alt='Spiritual - Reading the Scriptures Outside'>
-        <p>Content Here</p>
+        <p>Jesus increased in "favour with God." You can grow spiritually by showing your love for God and your willingness to live the gospel.</p>
     </div>
     <div>
         <h3>Social</h3>
         <img src='/2021/images/social-w1280x853.webp' alt='Social - Talking around a camp fire'>
-        <p>Content Here</p>
+        <p>Jesus grew in "favour with...man." You can grow socially by showing love to all people.</p>
     </div>
     <div>
         <h3>Physical</h3>
         <img src='/2021/images/physical-w1280x853.webp' alt='Physical - Tug-of-war'>
-        <p>Content Here</p>
+        <p>Jesus grew in "stature." You can learn to work and take care of your body, your mental health, and God's creations.</p>
     </div>
     <div>
         <h3>Intellectual</h3>
         <img src='/2021/images/intellectual-w1280x854.webp' alt='Intellectual - Studying together in the forest'>
-        <p>Content Here</p>
+        <p>Jesus grew in "wisdom." You can study in school, learn new skills, and improve your talents.</p>
     </div>
 </section>
