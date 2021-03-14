cb247cabcdac6ad84dc0856129709780dc65f7de
iii <blbaileyiii.naa@gmail.com>
2021-03-14 16:59:31 -0700

    a

--- web/2021/registration/index.php
+++ web/2021/registration/index.php
@@ -78,9 +78,9 @@ switch($action){
         break;
     default:
         // include $_SERVER['DOCUMENT_ROOT'] . '/camp2021/view/home.php';
-        $events = getEvents(2021);
-        $eventList = buildEventList($events);
-        $eventScript = buildEventScript($events);
+        //$events = getEvents(2021);
+        //$eventList = buildEventList($events);
+        //$eventScript = buildEventScript($events);
         include $_SERVER['DOCUMENT_ROOT'] . '/2021/view/registration.php';
 }
 
