ea80656144be5f33d0d0d7cf17f6baa5aa13a0d5
iii <blbaileyiii.naa@gmail.com>
2021-03-14 15:15:14 -0700

    a

--- web/2021/common/schedule-all.php
+++ web/2021/common/schedule-all.php
@@ -1,7 +1,7 @@
 <a class="anchor" id="schedule"></a>
 <section class="schedule">
-    <h2>Schedule</h2>
-    <ul>
+    <h2>Camp Schedules</h2>
+    <ul class="all-schedules">
         <li class="camp-yr-logo"><a href="/2021/?action=ywcamp#schedule" title=""><span class="logo-highlight logo-lg">YWCamp</span><span class="logo-sm">2021</span></a></li>
         <li class="camp-yr-logo"><a href="/2021/?action=ymcamp#schedule" title=""><span class="logo-highlight logo-lg">YMCamp</span><span class="logo-sm">2021</span></a></li>
         <li class="camp-yr-logo"><a href="/2021/?action=trek#schedule" title=""><span class="logo-highlight logo-lg">Trek</span><span class="logo-sm">2021</span></a></li>
