651d5dbf6a553e98ca484fabb3517b9d4c3ad194
iii <blbaileyiii.naa@gmail.com>
2021-03-14 11:05:17 -0700

    a

--- web/2021/common/pillars.php
+++ web/2021/common/pillars.php
@@ -4,19 +4,22 @@
     <img src='/2021/images/spiritual_social_intellectual_physical_cyd_logo.webp' alt='Church of Jesus Christ of Latter-Day Saints Youth Pillars'>
     <div>
         <h3>Spiritual</h3>
-        <img src='/2021/images/spiritual-w1280x854.webp' alt='Reading the Scriptures'>
+        <img src='/2021/images/spiritual-w1280x854.webp' alt='Spiritual - Reading the Scriptures Outside'>
         <p>Content Here</p>
     </div>
     <div>
         <h3>Social</h3>
+        <img src='/2021/images/social-w1280x853.webp' alt='Social - Talking around a camp fire'>
         <p>Content Here</p>
     </div>
     <div>
         <h3>Physical</h3>
+        <img src='/2021/images/physical-w1280x853.webp' alt='Physical - Tug-of-war'>
         <p>Content Here</p>
     </div>
     <div>
         <h3>Intellectual</h3>
+        <img src='/2021/images/intellectual-w1280x854.webp' alt='Intellectual - Studying together in the forest'>
         <p>Content Here</p>
     </div>
 </section>
