92972fc44f1fcf4c9f33f49037b4e33f3a71cf57
iii <blbaileyiii.naa@gmail.com>
2021-03-14 22:19:42 -0700

    a

--- web/2021/common/header-nav.php
+++ web/2021/common/header-nav.php
@@ -2,18 +2,19 @@
    <span class="logo-lg">Camp</span><span class="logo-highlight logo-sm">2021</span>
 </div>
 <ul class="nav-ul">
-    <li class="ham"><a id="toggleMenu" href="#">☰ Menu</a></li>
-    <li><a href="/2021/" title="Hacienda Heights Stake Youth Camps Home">Home</a></li>
-    <li><a href="/2021/?action=ywcamp" title="">YW Camp</a></li>
-    <li><a href="/2021/?action=ymcamp" title="">YM Camp</a></li>
-    <li><a href="/2021/?action=trek" title="">Trek</a></li>
-    <li><a href="#about" title="">About</a></li>
-    <li><a href="#schedule" title="">Schedule</a></li>
-    <li><a href="#focus" title="">The Focus</a></li>
-    <li><a href="#gallery" title="">Gallery</a></li>
-    <li><a href="#faq" title="">FAQ</a></li>
-    <li><a href="#equipment" title="">Equipment</a></li>
-    <li><a href="https://www.churchofjesuschrist.org/callings/church-safety-and-health/protecting-children-and-youth?lang=eng" title="Leadership Training" target="_blank" rel="noreferrer">Leadership Training</a></li>
-    <li><a href="/2021/registration" title="">Registration</a></li>
+    <li class="nav item ham"><a id="toggleMenu" href="#">☰ Menu</a></li>
+    <li class="nav item"><a href="/2021/" title="Hacienda Heights Stake Youth Camps Home">Home</a></li>
+    <li class="nav item"><a href="/2021/?action=ywcamp" title="">YW Camp</a></li>
+    <li class="nav item"><a href="/2021/?action=ymcamp" title="">YM Camp</a></li>
+    <li class="nav item"><a href="/2021/?action=trek" title="">Trek</a></li>
+    <li class="nav item"><a href="#about" title="">About</a></li>
+    <li class="nav item"><a href="#schedule" title="">Schedule</a></li>
+    <li class="nav item"><a href="#focus" title="">The Focus</a></li>
+    <li class="nav item"><a href="#gallery" title="">Gallery</a></li>
+    <li class="nav item"><a href="#faq" title="">FAQ</a></li>
+    <li class="nav item"><a href="#equipment" title="">Equipment</a></li>
+    <li class="nav item"><a href="https://www.churchofjesuschrist.org/callings/church-safety-and-health/protecting-children-and-youth?lang=eng" title="Leadership Training" target="_blank" rel="noreferrer">Leadership Training</a></li>
+    <li class="nav item"><a href="/2021/registration" title="">Registration</a></li>
 </ul>
+<script src="js/nav.js"></script>
 
