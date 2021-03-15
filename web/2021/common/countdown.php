7f4e1d37d2f37b113f029d14029d6c0e952b1722
iii <blbaileyiii.naa@gmail.com>
2021-03-14 20:20:07 -0700

    a

--- web/2021/common/countdown.php
+++ web/2021/common/countdown.php
@@ -1,4 +1,4 @@
-<section class="countdown-section parallax">
+<section class="parallax-countdown-section">
     <div class="filter">
         <h1>Hacienda Heights, CA Stake</h1>
         <div class="countdown-data"></div>
