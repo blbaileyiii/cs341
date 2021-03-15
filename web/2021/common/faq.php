dcf47119e6fcd17fc789e4adb4fce0a15e5989a7
iii <blbaileyiii.naa@gmail.com>
2021-03-15 12:06:42 -0700

    a

--- web/2021/common/faq.php
+++ web/2021/common/faq.php
@@ -5,6 +5,10 @@
     <div class="collapsible-content">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
+    <button type="button" class="collapsible">Open Collapsible</button>
+    <div class="collapsible-content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
 </section>
 <script>
     var coll = document.getElementsByClassName("collapsible");
