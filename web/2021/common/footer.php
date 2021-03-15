f750ebc6829f1bb7cf76fcafc626b94272bc7d09
iii <blbaileyiii.naa@gmail.com>
2021-03-15 11:22:34 -0700

    a

--- web/2021/common/footer.php
+++ web/2021/common/footer.php
@@ -1,4 +1,3 @@
-
 <div id="page-info">
       <p>&copy; <span id="currYear"></span> Copyright Hacienda Heights, CA LDS Stake Leadership. All Rights Reserved
         <br><span id="lastUpdated" class="lastUpdated"><?php echo "Last Updated: " . date("j F, Y", getlastmod());?></span>
