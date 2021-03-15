c947a81af18f073f54a4535dcc779910ac795e6a
iii <blbaileyiii.naa@gmail.com>
2021-03-15 11:43:46 -0700

    a

--- web/2021/common/guidelines.php
+++ web/2021/common/guidelines.php
@@ -2,4 +2,7 @@
 <section class="guidelines">
     <h2>Camp 2021</h2>
     <p>Camps should be planned to strengthen testimonies and foster personal growth, not just to be recreational activities. A camp </p>
+    <h3>Useful Links</h3>
+    <h3>Learn More</h3>
+    <h3>Additional Resources</h3>
 </section>
