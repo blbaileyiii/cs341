c795ce5f1d402b43d7fb34ee283fd99db940491d
iii <blbaileyiii.naa@gmail.com>
2021-03-15 12:14:37 -0700

    a

--- web/2021/common/faq.php
+++ web/2021/common/faq.php
@@ -1,14 +1,21 @@
 <a class="anchor" id="faq"></a>
 <section class="faq">
     <h2>F.A.Q.</h2>
-    <button type="button" class="collapsible">Open Collapsible</button>
-    <div class="collapsible-content">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    <button type="button" class="collapsible">Open Collapsible</button>
-    <div class="collapsible-content">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
+    <ul>
+        <li>
+            <button type="button" class="collapsible">Open Collapsible</button>
+            <div class="collapsible-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </li>
+        <li>
+            <button type="button" class="collapsible">Open Collapsible</button>
+            <div class="collapsible-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </li>
+    </ul>
+    
 </section>
 <script>
     var coll = document.getElementsByClassName("collapsible");
