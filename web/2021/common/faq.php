3ccd7cbc4f4d25aeacce14e1e5ebb635d889ae50
iii <blbaileyiii.naa@gmail.com>
2021-03-15 12:05:43 -0700

    a

--- web/2021/common/faq.php
+++ web/2021/common/faq.php
@@ -2,23 +2,23 @@
 <section class="faq">
     <h2>F.A.Q.</h2>
     <button type="button" class="collapsible">Open Collapsible</button>
-    <div class="content">
+    <div class="collapsible-content">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
 </section>
 <script>
-var coll = document.getElementsByClassName("collapsible");
-var i;
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("faq-active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
     }
-  });
-}
 </script>
