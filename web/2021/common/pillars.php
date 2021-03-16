cad4c1a66ccdef428be5a6cd4039ce56bcd82522
iii <blbaileyiii.naa@gmail.com>
2021-03-15 23:05:22 -0700

    a

--- web/2021/common/pillars.php
+++ web/2021/common/pillars.php
@@ -4,24 +4,32 @@
         <h2>The Focus</h2>
         <div class="pillars"><img class="responsive img-sm" src='/2021/images/spiritual_social_intellectual_physical_cyd_logo.webp' alt='Church of Jesus Christ of Latter-Day Saints Youth Pillars'></div>
         <div class="pillars-content">
-            <h3>Spiritual</h3>
+            <div>
+                <h3>Spiritual</h3>
+                <p>Jesus increased in "favour with God." You can grow spiritually by showing your love for God and your willingness to live the gospel.</p>
+            </div>
             <img class="responsive" src='/2021/images/spiritual-w1280x854.webp' alt='Spiritual - Reading the Scriptures Outside'>
-            <p>Jesus increased in "favour with God." You can grow spiritually by showing your love for God and your willingness to live the gospel.</p>
         </div>
         <div class="pillars-content">
-            <h3>Social</h3>
+            <div>
+                <h3>Social</h3>
+                <p>Jesus grew in "favour with...man." You can grow socially by showing love to all people.</p>
+            </div>
             <img class="responsive" src='/2021/images/social-w1280x853.webp' alt='Social - Talking around a camp fire'>
-            <p>Jesus grew in "favour with...man." You can grow socially by showing love to all people.</p>
         </div>
         <div class="pillars-content">
-            <h3>Physical</h3>
+            <div>
+                <h3>Physical</h3>
+                <p>Jesus grew in "stature." You can learn to work and take care of your body, your mental health, and God's creations.</p>
+            </div>
             <img class="responsive" src='/2021/images/physical-w1280x853.webp' alt='Physical - Tug-of-war'>
-            <p>Jesus grew in "stature." You can learn to work and take care of your body, your mental health, and God's creations.</p>
         </div>
         <div class="pillars-content">
-            <h3>Intellectual</h3>
+            <div>
+                <h3>Intellectual</h3>
+                <p>Jesus grew in "wisdom." You can study in school, learn new skills, and improve your talents.</p>
+            </div>
             <img class="responsive" src='/2021/images/intellectual-w1280x854.webp' alt='Intellectual - Studying together in the forest'>
-            <p>Jesus grew in "wisdom." You can study in school, learn new skills, and improve your talents.</p>
         </div>
     </div>
 </section>
