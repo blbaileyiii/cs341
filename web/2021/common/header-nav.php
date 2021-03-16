7535246578a232cf3901feb56aad4c473d7cf358
iii <blbaileyiii.naa@gmail.com>
2021-03-16 08:15:55 -0700

    a

--- web/2021/common/header-nav.php
+++ web/2021/common/header-nav.php
@@ -5,17 +5,17 @@
     <span class="nav item ham"><a id="toggleMenu" href="javascript:void(0);">☰ Menu</a></span>
     <ul class="nav-ul">        
         <li class="nav item"><a href="/2021/" title="Hacienda Heights Stake Youth Camps Home">Home</a></li>
-        <li class="nav item"><a href="/2021/?action=ywcamp" title="">YW Camp</a></li>
-        <li class="nav item"><a href="/2021/?action=ymcamp" title="">YM Camp</a></li>
-        <li class="nav item"><a href="/2021/?action=trek" title="">Trek</a></li>
-        <li class="nav item"><a href="#about" title="">About</a></li>
-        <li class="nav item"><a href="#schedule" title="">Schedule</a></li>
-        <li class="nav item"><a href="#focus" title="">The Focus</a></li>
-        <li class="nav item"><a href="#gallery" title="">Gallery</a></li>
-        <li class="nav item"><a href="#faq" title="">FAQ</a></li>
-        <li class="nav item"><a href="#equipment" title="">Equipment</a></li>
+        <li class="nav item"><a href="/2021/?action=ywcamp" title="YW Camp">YW Camp</a></li>
+        <li class="nav item"><a href="/2021/?action=ymcamp" title="YM Camp">YM Camp</a></li>
+        <li class="nav item"><a href="/2021/?action=trek" title="Trek">Trek</a></li>
+        <li class="nav item"><a href="#about" title="About">About</a></li>
+        <li class="nav item"><a href="#schedule" title="Schedule">Schedule</a></li>
+        <li class="nav item"><a href="#focus" title="The Focus">The Focus</a></li>
+        <li class="nav item"><a href="#gallery" title="Gallery">Gallery</a></li>
+        <li class="nav item"><a href="#faq" title="FAQ">FAQ</a></li>
+        <li class="nav item"><a href="#equipment" title="Equipment">Equipment</a></li>
         <li class="nav item"><a href="https://www.churchofjesuschrist.org/callings/church-safety-and-health/protecting-children-and-youth?lang=eng" title="Leadership Training" target="_blank" rel="noreferrer">Leadership Training</a></li>
-        <li class="nav item"><a href="https://www.hhscamps.com//2021/registration" title="">Registration</a></li>
+        <li class="nav item"><a href="https://www.hhscamps.com/2021/registration" title="Registration">Registration</a></li>
     </ul>
 </div>
 <script src="/2021/js/nav.js"></script>
