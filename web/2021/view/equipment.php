d296fe976160ee18f01ce0fdbf8d49616b8c5943
iii <blbaileyiii.naa@gmail.com>
2021-03-16 16:14:00 -0700

    a

--- web/2021/view/equipment.php
+++ web/2021/view/equipment.php
@@ -15,102 +15,87 @@
         <p>Duffle bags or small suitcases are preferred for packing.</p>
         <p>(Please label all of your belongings with your name and ward name)</p>
         <ul>
-            <li><h2>Clothing<h2>
+            <li><h2>Clothing</h2>
                 <ul>
                     <li>3-4 pair of long pants</li>
-                    <li>2-3 pair walking shorts/capris – Must be
-                    knee length or longer</li>
+                    <li>2-3 pair walking shorts/capris – Must be knee length or longer</li>
                     <li>5-6 shirts</li>
                     <li>6 undergarments</li>
                     <li>6 pair of socks</li>
-                    <li>warm coat/light raingear</li>
-                    <li>modest, one piece swimsuit</li>
-                    <li>sturdy shoes/hiking boots – must be
-                    broken in (no open-toed shoes, except for
-                    shower use)</li>
-                    <li>modest sleepwear/pajamas/sweatpants</li>
-                    <li>hat</li>
+                    <li>Warm coat/light raingear</li>
+                    <li>Modest, one piece swimsuit</li>
+                    <li>Sturdy shoes/hiking boots – must be broken in (no open-toed shoes, except for shower use)</li>
+                    <li>Modest sleepwear/pajamas/sweatpants</li>
+                    <li>Hat</li>
                 </ul>
             </li>
             <li><h2>Toiletries</h2>
                 <ul>
-                    <li>soap</li>
-                    <li>shampoo, conditioner</li>
-                    <li>comb / brush</li>
-                    <li>toothbrush and toothpaste</li>
-                    <li>deodorant</li>
+                    <li>Soap</li>
+                    <li>Shampoo, conditioner</li>
+                    <li>Comb / brush</li>
+                    <li>Toothbrush and toothpaste</li>
+                    <li>Deodorant</li>
                     <li>2 towels (bath and swimming)</li>
-                    <li>lip balm</li>
-                    <li>sunscreen</li>
-                    <li>tissues</li>
-                    <li>feminine products for female staff (even if
-                    you don’t think you’ll need them)</li>
+                    <li>Lip balm</li>
+                    <li>Sunscreen</li>
+                    <li>Tissues</li>
+                    <li>Feminine products for female staff (even if you don’t think you’ll need them)</li>
                 </ul>
             </li>
             <li><h2>Bedding</h2>
                 <ul>
-                    <li>warm, adult-size sleeping bag or bedroll
-                    (keep bedding clean during transit by covering with
-                    a large trash bag labeled with name and ward)</li>
-                    <li>Cabin Leaders – air mattress or foam pad
-                    (otherwise you will sleep on hard, wood platform)</li>
-                    <li>Priesthood – cot/pad for those sleeping on
-                    tent platforms</li>
-                    <li>pillow</li>
-                    <li>extra garbage bag (for bedding on return trip)</li>
+                    <li>Warm, adult-size sleeping bag or bedroll (keep bedding clean during transit by covering with a large trash bag labeled with name and ward)</li>
+                    <li>Cabin Leaders – air mattress or foam pad (otherwise you will sleep on hard, wood platform)</li>
+                    <li>Priesthood – cot/pad for those sleeping on tent platforms</li>
+                    <li>Pillow</li>
+                    <li>Extra garbage bag (for bedding on return trip)</li>
                 </ul>
             </li>
             <li><h2>Medication</h2>
-                <p>Please bring all medication needed for your
-                stay at camp and keep it securely stored in
-                your suitcase or carried with you.<p>
+                <p>Please bring all medication needed for your stay at camp and keep it securely stored in your suitcase or carried with you.<p>
             </li>
             <li><h2>Miscellaneous</h2>
                 <ul>
-                    <li>sack lunch for Monday</li>
-                    <li>scriptures (non-electronic), journal</li>
-                    <li>camp manual (used for certification)</li>
-                    <li>camp binder</li>
-                    <li>flashlight/headlamp (extra batteries & bulb)</li>
+                    <li>Sack lunch for Monday</li>
+                    <li>Scriptures (non-electronic), journal</li>
+                    <li>Camp manual (used for certification)</li>
+                    <li>Camp binder</li>
+                    <li>Flashlight/headlamp (extra batteries & bulb)</li>
                 </ul>
             </li>
             <li><h2>Water Hydration System</h2>
-                <p>Each leader should come to camp with an
-                appropriate hydration system. Examples of
-                hydration systems include: hydration packs,
-                canteens, water bottles, and flasks. Bring at
-                least 1.5 - 3 liters.</p>
+                <p>Each leader should come to camp with an appropriate hydration system. Examples of hydration systems include: hydration packs, canteens, water bottles, and flasks. Bring at least 1.5 - 3 liters.</p>
             </li>
             <li><h2>Optional Items</h2>
                 <ul>
-                    <li>insect repellent</li>
-                    <li>robe and/or slippers</li>
-                    <li>wristwatch</li>
-                    <li>flip flops (for shower use ONLY)</li>
-                    <li>camera</li>
-                    <li>shaver</li>
-                    <li>nail clippers/file</li>
-                    <li>blow dryer/curling iron</li>
-                    <li>make-up</li>
-                    <li>stationary, stamps, pen</li>
-                    <li>notebook and pencil</li>
-                    <li>small first-aid kit</li>
-                    <li>stadium seat and/or camping chair</li>
-                    <li>wetsuit</li>
-                    <li>scissors, tape, sharpie marker</li>
-                    <li>any items needed to help fulfill your
-                    assignment at camp</li>
+                    <li>Insect repellent</li>
+                    <li>Robe and/or slippers</li>
+                    <li>Wristwatch</li>
+                    <li>Flip flops (for shower use ONLY)</li>
+                    <li>Camera</li>
+                    <li>Shaver</li>
+                    <li>Nail clippers/file</li>
+                    <li>Blow dryer/curling iron</li>
+                    <li>Make-up</li>
+                    <li>Stationary, stamps, pen</li>
+                    <li>Notebook and pencil</li>
+                    <li>Small first-aid kit</li>
+                    <li>Stadium seat and/or camping chair</li>
+                    <li>Wetsuit</li>
+                    <li>Scissors, tape, sharpie marker</li>
+                    <li>Any items needed to help fulfill your assignment at camp</li>
                 </ul>
             </li>
         </ul>
         <h2>DO NOT BRING</h2>
         <ul>
-            <li>sleeveless shirts or tank tops</li>
-            <li>skin tight clothing</li>
-            <li>shorts that do not come to the knee</li>
-            <li>candy</li>
-            <li>comic books, romance novels</li>
-            <li>playing cards</li>
+            <li>Sleeveless shirts or tank tops</li>
+            <li>Skin tight clothing</li>
+            <li>Shorts that do not come to the knee</li>
+            <li>Candy</li>
+            <li>Comic books, romance novels</li>
+            <li>Playing cards</li>
         </ul>
         <h2>Hydration Requirements</h2>
         <p>We all should constantly manage her hydration level during all camp activities. Daytime temperatures, dry climate, and elevation, coupled with strenuous outdoor activities can increase how quickly we may become dehydrated. Although water is available at the lodge during mealtimes, as well as before and after activity times, each participant MUST carry water for all activities conducted away from the lodge (e.g. hikes, waterfront, climbing, etc.)</p>
