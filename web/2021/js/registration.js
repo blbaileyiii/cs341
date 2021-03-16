01fb61ee9c96595acde404e9aa2eedfd3bb2b101
iii <blbaileyiii.naa@gmail.com>
2021-03-16 10:38:40 -0700

    a

--- web/2021/js/registration.js
+++ web/2021/js/registration.js
@@ -2,9 +2,8 @@ document.getElementById('participantDOB').addEventListener('blur', function() {
     let dOB = new Date(this.valueAsNumber);
     let ageInput = document.getElementById('participantAge');
     
-        let diff_ms = Date.now() - dOB.getTime();
-        let age_dt = new Date(diff_ms); 
-      
-        ageInput.value = Math.abs(age_dt.getUTCFullYear() - 1970);
-    }
+    let diff_ms = Date.now() - dOB.getTime();
+    let age_dt = new Date(diff_ms); 
+    
+    ageInput.value = Math.abs(age_dt.getUTCFullYear() - 1970);
 });
