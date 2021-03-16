0d738c4ed86e8b1672fde57634bad0ae871a21c1
iii <blbaileyiii.naa@gmail.com>
2021-03-15 22:11:51 -0700

    a

--- web/2021/common/about.php
+++ web/2021/common/about.php
@@ -1,8 +1,10 @@
 <a class="anchor" id="about"></a>
 <section class="about">
-    <div class="section-center">
-        <h2><span class="logo-lg">Camp</span><span class="logo-highlight logo-sm">2021</span> Theme</h2>
-        <p>"And Jesus increased in wisdom and stature, and in favour with God and man." (<a href="https://www.churchofjesuschrist.org/study/scriptures/nt/luke/2.52?lang=eng&clang=eng#p52" title="Luke 2:52" target="_blank" rel="noreferrer">Luke 2:52</a>) </p>
+    <div class="section-center about-content">
+        <div>
+            <h2><span class="logo-lg">Camp</span><span class="logo-highlight logo-sm">2021</span> Theme</h2>
+            <p>"And Jesus increased in wisdom and stature, and in favour with God and man." (<a href="https://www.churchofjesuschrist.org/study/scriptures/nt/luke/2.52?lang=eng&clang=eng#p52" title="Luke 2:52" target="_blank" rel="noreferrer">Luke 2:52</a>) </p>
+        </div>
         <div class="details">
             <span>WHERE</span>
             <span>WHEN</span>
@@ -13,17 +15,19 @@
             <span>TREK: Riley's Farm</span>
             <span>October 22-23 (Fri-Sat)</span>
         </div>
-        <h2>Purpose of Camp</h2>
-        <p>The purposes of youth camps are to help youth build faith in Jesus Christ, strengthen their testimonies, develop talents, make new friends, and have fun with youth who share similar beliefs and standards.</p>
+        <div>
+            <h2>Purpose of Camp</h2>
+            <p>The purposes of youth camps are to help youth build faith in Jesus Christ, strengthen their testimonies, develop talents, make new friends, and have fun with youth who share similar beliefs and standards.</p>
+        </div>        
         <ul>
-        <li>Participating in service projects that bless others and build community relationships.</li>
-        <li>Developing talents and appreciation for cultural arts.</li>
-        <li>Improving fitness and learning sportsmanship.</li>
-        <li>Gaining education and vocational training.</li>
-        <li>Celebrating special occasions and commemorating Church or local historical events.</li>
-        <li>Developing leadership skills.</li>
-        <li>Developing self-reliance.</li>
-        <li>Participating in missionary work, retention, activation, temple work, and family history work.</li>
+            <li>Participating in service projects that bless others and build community relationships.</li>
+            <li>Developing talents and appreciation for cultural arts.</li>
+            <li>Improving fitness and learning sportsmanship.</li>
+            <li>Gaining education and vocational training.</li>
+            <li>Celebrating special occasions and commemorating Church or local historical events.</li>
+            <li>Developing leadership skills.</li>
+            <li>Developing self-reliance.</li>
+            <li>Participating in missionary work, retention, activation, temple work, and family history work.</li>
         </ul>
     </div>
 </section>
