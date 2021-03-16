bbc8053fb7d0ae752f6d130bd85f45ecc85f2564
iii <blbaileyiii.naa@gmail.com>
2021-03-16 07:14:09 -0700

    a

--- web/2021/index.php
+++ web/2021/index.php
@@ -5,11 +5,11 @@
  */
 session_start();
 // Get the database connection file
-require_once $_SERVER['DOCUMENT_ROOT'] . '/2021/libraries/connections.php';
+require_once '/2021/libraries/connections.php';
 // Get the registration model for use as needed
-require_once $_SERVER['DOCUMENT_ROOT'] . '/2021/models/reg-model.php';
+require_once '/2021/models/reg-model.php';
 // Get the fxs for valiation and file building
-require_once $_SERVER['DOCUMENT_ROOT'] . '/2021/libraries/fx.php';
+require_once '/2021/libraries/fx.php';
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
@@ -18,18 +18,18 @@ if ($action == NULL) {
 
 switch($action){
     case 'ymcamp':
-        include $_SERVER['DOCUMENT_ROOT'] . '/2021/view/ymcamp.php';
+        include '/2021/view/ymcamp.php';
         break;
     case 'ywcamp':
-        include $_SERVER['DOCUMENT_ROOT'] . '/2021/view/ywcamp.php';
+        include '/2021/view/ywcamp.php';
         break;
     case 'trek':
-        include $_SERVER['DOCUMENT_ROOT'] . '/2021/view/trek.php';
+        include '/2021/view/trek.php';
         break;
     default:
         $events = getEvents(2021);
         $aboutHTML = buildAboutHTML($events);
         $contactsHTML = buildContactsHTML($events);
-        include $_SERVER['DOCUMENT_ROOT'] . '/2021/view/home.php';
+        include '/2021/view/home.php';
 }
 ?>
