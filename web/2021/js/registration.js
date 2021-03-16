eff96706e47958042d53a63622f3296fe1c22868
iii <blbaileyiii.naa@gmail.com>
2021-03-16 11:28:27 -0700

    a

--- web/2021/js/registration.js
+++ web/2021/js/registration.js
@@ -3,7 +3,13 @@ document.getElementById('participantDOB').addEventListener('change', function() 
     let ageInput = document.getElementById('participantAge');
     
     let diff_ms = Date.now() - dOB.getTime();
-    let age_dt = new Date(diff_ms); 
+    let age_dt = new Date(diff_ms);
+
+    if(isNaN(age_dt)){
+        ageInput.value = "INVALID BIRTHDAY";
+    } else {
+        ageInput.value = Math.abs(age_dt.getUTCFullYear() - 1970);
+    }
     
-    ageInput.value = Math.abs(age_dt.getUTCFullYear() - 1970);
+    
 });
