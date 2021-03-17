2f86cf4b30cb6d33255e59b7a7e4eda2651294a1
iii <blbaileyiii.naa@gmail.com>
2021-03-17 10:45:11 -0700

    a

--- web/2021/view/attributions.php
+++ web/2021/view/attributions.php
@@ -15,23 +15,23 @@
         <div class="attributions">
             <figure>
                 <a href="https://unsplash.com/photos/Yizrl9N_eDA" target="_blank" rel="noreferrer"><img src="/2021/images/hike-w1280x853.webp" alt="Hiking"></a>
-                <figcaption>Photo by <a href="https://unsplash.com@toomastartes" target="_blank" rel="noreferrer">Toomas Tartes</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
+                <figcaption>Photo by <a href="https://unsplash.com/@toomastartes" target="_blank" rel="noreferrer">Toomas Tartes</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
                 <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/equipment-w1280x853.webp" alt="Camping Equipment"></a>
-                <figcaption>Photo by <a href="https://unsplash.com@nineteen" target="_blank" rel="noreferrer">Andrew Ly</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
+                <figcaption>Photo by <a href="https://unsplash.com/@nineteen" target="_blank" rel="noreferrer">Andrew Ly</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
                 <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/social-w1280x853.webp" alt="Social/Camp Fire"></a>
-                <figcaption>Photo by <a href="https://unsplash.com@tegan" target="_blank" rel="noreferrer">Tegan Mierle</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
+                <figcaption>Photo by <a href="https://unsplash.com/@tegan" target="_blank" rel="noreferrer">Tegan Mierle</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
                 <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/physical-w1280x853.webp" alt="Tug of War"></a>
-                <figcaption>Photo by <a href="https://unsplash.com@hagalnaud" target="_blank" rel="noreferrer">Anna Samoylova</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
+                <figcaption>Photo by <a href="https://unsplash.com/@hagalnaud" target="_blank" rel="noreferrer">Anna Samoylova</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
                 <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/spiritual-w1280x854.webp" alt="Scriptures/Outdoors"></a>
-                <figcaption>Photo by <a href="https://unsplash.com@benwhitephotography" target="_blank" rel="noreferrer">Ben White</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
+                <figcaption>Photo by <a href="https://unsplash.com/@benwhitephotography" target="_blank" rel="noreferrer">Ben White</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
                 <a href="https://www.pexels.com/photo/concentrated-multiethnic-female-students-reading-article-together-in-park-5965826" target="_blank" rel="noreferrer"><img src="/2021/images/intellectual-w1280x854.webp" alt="Outdoor Learning"></a>
@@ -39,20 +39,20 @@
             </figure>
             <figure>
                 <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/growth-path-w1280x1707.webp" alt="Path Through Growing Trees"></a>
-                <figcaption>Photo by <a href="https://unsplash.com@jane_palash" target="_blank" rel="noreferrer">Jane Palash</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
+                <figcaption>Photo by <a href="https://unsplash.com/@jane_palash" target="_blank" rel="noreferrer">Jane Palash</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <!--
             <ul>
-                <li>Hiking: Photo by <a href="https://unsplash.com@toomastartes">Toomas Tartes</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
-                <li>Hiking Joy: Photo by <a href="https://unsplash.com@chiro">photo-nic.co.uk nic</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
-                <li>Camping Equipment: Photo by <a href="https://unsplash.com@nineteen">Andrew Ly</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
-                <li>Trek: Photo by <a href="https://unsplash.com@randyfath">Randy Fath</a> on <a href="/s/photos/camping">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
-                <li>Rock Climbing/ Physical: Photo by <a href="https://unsplash.com@huchenme">Hu Chen</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
-                <li>Social/Camp Fire: Photo by <a href="https://unsplash.com@tegan">Tegan Mierle</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
-                <li>Tug of War: Photo by <a href="https://unsplash.com@hagalnaud">Anna Samoylova</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
-                <li>Scriptures/Outdoors: Photo by <a href="https://unsplash.com@benwhitephotography">Ben White</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Hiking: Photo by <a href="https://unsplash.com/@toomastartes">Toomas Tartes</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Hiking Joy: Photo by <a href="https://unsplash.com/@chiro">photo-nic.co.uk nic</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Camping Equipment: Photo by <a href="https://unsplash.com/@nineteen">Andrew Ly</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Trek: Photo by <a href="https://unsplash.com/@randyfath">Randy Fath</a> on <a href="/s/photos/camping">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Rock Climbing/ Physical: Photo by <a href="https://unsplash.com/@huchenme">Hu Chen</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Social/Camp Fire: Photo by <a href="https://unsplash.com/@tegan">Tegan Mierle</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Tug of War: Photo by <a href="https://unsplash.com/@hagalnaud">Anna Samoylova</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Scriptures/Outdoors: Photo by <a href="https://unsplash.com/@benwhitephotography">Ben White</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
                 <li>Outdoor Learning: Photo by Charlotte May from Pexels https://www.pexels.com/@charlotte-may?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels  https://www.pexels.com/photo/concentrated-multiethnic-female-students-reading-article-together-in-park-5965826/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels</li>
-                <li>Growing and Path: Photo by <a href="https://unsplash.com@jane_palash">Jane Palash</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
+                <li>Growing and Path: Photo by <a href="https://unsplash.com/@jane_palash">Jane Palash</a> on <a href="https://unsplash.com">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></li>
                 <li>amazing view: https://www.pexels.com/photo/scenic-photo-of-rocky-mountain-during-golden-hour-2440078/ https://www.pexels.com/@ian-beckley-1278367?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels Photo by Ian Beckley from Pexels</li>
                 <li>hiking: https://www.pexels.com/photo/group-of-person-walking-in-mountain-1365425/ https://www.pexels.com/@sanmane?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels Photo by Eric Sanman from Pexels</li>
             </ul>
