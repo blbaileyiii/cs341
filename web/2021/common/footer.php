bd80d8812f5a0e90a582364520251d4c53d92b8b
iii <blbaileyiii.naa@gmail.com>
2021-03-17 08:21:56 -0700

    a

--- web/2021/common/footer.php
+++ web/2021/common/footer.php
@@ -1,6 +1,6 @@
 <div id="page-info">
   <p>&copy; <span id="currYear"></span> Copyright Hacienda Heights, CA LDS Stake Leadership. All Rights Reserved
-  <br>Created by: B L Bailey III - <a href = "mailto: hhscamps@gmail.com">Contact</a> || <span id="lastUpdated" class="lastUpdated"><?php echo "Last Updated: " . date("j F, Y", getlastmod());?></span></p>
+  <br>Created by: BL Bailey III - <a href = "mailto: hhscamps@gmail.com">Contact</a> || <span id="lastUpdated" class="lastUpdated"><?php echo "Last Updated: " . date("j F, Y", getlastmod());?></span></p>
   <hr>
   <p>"This is not an official website of The Church of Jesus Christ of Latter-day Saints."</p>
 </div>
