8088fbff018986f06ec17a607a0434d57ee0d62f
iii <blbaileyiii.naa@gmail.com>
2021-03-17 11:05:07 -0700

    a

--- web/2021/view/attributions.php
+++ web/2021/view/attributions.php
@@ -18,19 +18,19 @@
                 <figcaption>Photo by <a href="https://unsplash.com/@toomastartes" target="_blank" rel="noreferrer">Toomas Tartes</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/equipment-w1280x853.webp" alt="Camping Equipment"></a>
+                <a href="https://unsplash.com/photos/bQl2kRQyUE8" target="_blank" rel="noreferrer"><img src="/2021/images/equipment-w1280x853.webp" alt="Camping Equipment"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@nineteen" target="_blank" rel="noreferrer">Andrew Ly</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/social-w1280x853.webp" alt="Social/Camp Fire"></a>
+                <a href="https://unsplash.com/photos/fDostElVhN8" target="_blank" rel="noreferrer"><img src="/2021/images/social-w1280x853.webp" alt="Social/Camp Fire"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@tegan" target="_blank" rel="noreferrer">Tegan Mierle</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/physical-w1280x853.webp" alt="Tug of War"></a>
+                <a href="https://unsplash.com/photos/w55SpMmoPgE" target="_blank" rel="noreferrer"><img src="/2021/images/physical-w1280x853.webp" alt="Tug of War"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@hagalnaud" target="_blank" rel="noreferrer">Anna Samoylova</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/spiritual-w1280x854.webp" alt="Scriptures/Outdoors"></a>
+                <a href="https://unsplash.com/photos/gGbuETcoKjw" target="_blank" rel="noreferrer"><img src="/2021/images/spiritual-w1280x854.webp" alt="Scriptures/Outdoors"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@benwhitephotography" target="_blank" rel="noreferrer">Ben White</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
@@ -38,7 +38,7 @@
                 <figcaption>Photo by <a href="https://www.pexels.com/@charlotte-may?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels" target="_blank" rel="noreferrer">Charlotte May</a> from <a href="https://www.pexels.com/" target="_blank" rel="noreferrer">Pexels</a> - <a href="https://www.pexels.com/license/" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="" target="_blank" rel="noreferrer"><img src="/2021/images/growth-path-w1280x1707.webp" alt="Path Through Growing Trees"></a>
+                <a href="https://unsplash.com/photos/i4Fv22LzcQU" target="_blank" rel="noreferrer"><img src="/2021/images/growth-path-w1280x1707.webp" alt="Path Through Growing Trees"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@jane_palash" target="_blank" rel="noreferrer">Jane Palash</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <!--
