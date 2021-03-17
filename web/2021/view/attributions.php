19118d8317f63f8bcb072c4cc109b4a11e93a29e
iii <blbaileyiii.naa@gmail.com>
2021-03-17 09:10:12 -0700

    a

--- web/2021/view/attributions.php
+++ web/2021/view/attributions.php
@@ -12,20 +12,58 @@
     </header>    
     <main>    
         <h1><?php echo $page; ?></h1>
-        <ul>
-            <li>Hiking: Photo by <a href="https://unsplash.com/@toomastartes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Toomas Tartes</a> on <a href="/s/photos/hiking?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>Hiking Joy: Photo by <a href="https://unsplash.com/@chiro?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">photo-nic.co.uk nic</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>Camping Equipment: Photo by <a href="https://unsplash.com/@nineteen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andrew Ly</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>Trek: Photo by <a href="https://unsplash.com/@randyfath?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Randy Fath</a> on <a href="/s/photos/camping?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>Rock Climbing/ Physical: Photo by <a href="https://unsplash.com/@huchenme?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hu Chen</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>Social/Camp Fire: Photo by <a href="https://unsplash.com/@tegan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tegan Mierle</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>Tug of War: Photo by <a href="https://unsplash.com/@hagalnaud?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anna Samoylova</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>Scriptures/Outdoors: Photo by <a href="https://unsplash.com/@benwhitephotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ben White</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>Outdoor Learning: Photo by Charlotte May from Pexels https://www.pexels.com/@charlotte-may?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels  https://www.pexels.com/photo/concentrated-multiethnic-female-students-reading-article-together-in-park-5965826/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels</li>
-            <li>Growing and Path: Photo by <a href="https://unsplash.com/@jane_palash?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jane Palash</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></li>
-            <li>amazing view: https://www.pexels.com/photo/scenic-photo-of-rocky-mountain-during-golden-hour-2440078/ https://www.pexels.com/@ian-beckley-1278367?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels Photo by Ian Beckley from Pexels</li>
-            <li>hiking: https://www.pexels.com/photo/group-of-person-walking-in-mountain-1365425/ https://www.pexels.com/@sanmane?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels Photo by Eric Sanman from Pexels</li>
-        </ul>
+        <figure>
+            <img src="/2021/images/spiritual_social_intellectual_physical_cyd_logo.webp.webp" alt="">
+            <figcaption>Pillars...</figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/-w1280x853.webp" alt="">
+            <figcaption>Hiking: Photo by <a href="https://unsplash.com/@toomastartes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Toomas Tartes</a> on <a href="/s/photos/hiking?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/-w1280x853.webp" alt="">
+            <figcaption>Hiking Joy: Photo by <a href="https://unsplash.com/@chiro?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">photo-nic.co.uk nic</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/equipment-w1280x853.webp" alt="">
+            <figcaption>Camping Equipment: Photo by <a href="https://unsplash.com/@nineteen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andrew Ly</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/-w1280x853.webp" alt="">
+            <figcaption>Trek: Photo by <a href="https://unsplash.com/@randyfath?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Randy Fath</a> on <a href="/s/photos/camping?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/-w1280x853.webp" alt="">
+            <figcaption>Rock Climbing/ Physical: Photo by <a href="https://unsplash.com/@huchenme?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hu Chen</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/social-w1280x853.webp" alt="">
+            <figcaption>Social/Camp Fire: Photo by <a href="https://unsplash.com/@tegan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tegan Mierle</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/physical-w1280x853.webp" alt="">
+            <figcaption>Tug of War: Photo by <a href="https://unsplash.com/@hagalnaud?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anna Samoylova</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/spiritual-w1280x854.webp" alt="">
+            <figcaption>Scriptures/Outdoors: Photo by <a href="https://unsplash.com/@benwhitephotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ben White</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/intellectual-w1280x853.webp" alt="">
+            <figcaption>Outdoor Learning: Photo by Charlotte May from Pexels https://www.pexels.com/@charlotte-may?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels  https://www.pexels.com/photo/concentrated-multiethnic-female-students-reading-article-together-in-park-5965826/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels</figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/growth=path-w1280x1707.webp" alt="">
+            <figcaption>Growing and Path: Photo by <a href="https://unsplash.com/@jane_palash?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jane Palash</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/-w1280x853.webp" alt="">
+            <figcaption>amazing view: https://www.pexels.com/photo/scenic-photo-of-rocky-mountain-during-golden-hour-2440078/ https://www.pexels.com/@ian-beckley-1278367?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels Photo by Ian Beckley from Pexels</figcaption>
+        </figure>
+        <figure>
+            <img src="/2021/images/-w1280x853.webp" alt="">
+            <figcaption>hiking: https://www.pexels.com/photo/group-of-person-walking-in-mountain-1365425/ https://www.pexels.com/@sanmane?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels Photo by Eric Sanman from Pexels</figcaption>
+        </figure>
     </main>
     <footer>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/2021/common/footer.php'; ?>
