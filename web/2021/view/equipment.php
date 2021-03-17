ca4d09b7434501177038c5b41ebf3dbafd99a7ff
iii <blbaileyiii.naa@gmail.com>
2021-03-17 11:51:24 -0700

    a

--- web/2021/view/equipment.php
+++ web/2021/view/equipment.php
@@ -11,9 +11,9 @@
     <?php require $_SERVER['DOCUMENT_ROOT'] . '/2021/common/header-nav.php'; ?>
     </header>    
     <main>
-        <section class="equipment">
+        <div class="equipment">
             <div><img class="responsive" src="/2021/images/equipment-w1280x853.webp" alt="camping gear"></div>
-            <div class="equipment-list hidden">
+            <section class="equipment-list hidden">
                 <h1>Equipment List</h1>
                 <p>Duffle bags or small suitcases are preferred for packing.</p>
                 <p>(Please label all of your belongings with your name and ward name)</p>
@@ -100,8 +100,8 @@
                     <li>Comic books, romance novels</li>
                     <li>Playing cards</li>
                 </ul>
-            </div>
-            <div class="equipment-interactive hidden">
+            </section>
+            <section class="equipment-interactive hidden">
                 <h1>Equipment List Interactive</h1>
                 <p>Duffle bags or small suitcases are preferred for packing.</p>
                 <p>(Please label all of your belongings with your name and ward name)</p>
@@ -188,11 +188,11 @@
                     <li>Comic books, romance novels</li>
                     <li>Playing cards</li>
                 </ul>
-            </div>
+            </section>
             <h2>Hydration Requirements</h2>
             <p>We all should constantly manage her hydration level during all camp activities. Daytime temperatures, dry climate, and elevation, coupled with strenuous outdoor activities can increase how quickly we may become dehydrated. Although water is available at the lodge during mealtimes, as well as before and after activity times, each participant MUST carry water for all activities conducted away from the lodge (e.g. hikes, waterfront, climbing, etc.)</p>
             <p>The American College of Sports Medicine recommends drinking 1 liter of water per hour for moderate activity in moderate conditions. This is equivalent to two 16.9 oz/500ml bottles of water per hour. Everyone should come to camp with an appropriate hydration system. Examples of hydration systems include: hydration packs, canteens, reusable water bottles, and flasks. The required size of the system is dependent on camp level and activity.</p>
-        </section>
+        </div>
     </main>
     <footer>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/2021/common/footer.php'; ?>
