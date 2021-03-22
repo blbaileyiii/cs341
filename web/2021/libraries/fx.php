d3f5d77cec18ed1647b46734ede380f128eae6b7
iii <blbaileyiii.naa@gmail.com>
2021-03-22 15:37:17 -0700

    a

--- web/2021/libraries/fx.php
+++ web/2021/libraries/fx.php
@@ -42,6 +42,7 @@ function checkIsDate($chkVal, $format = 'Y-m-d')
 
 function checkBoolText($chkVal, $chkTxt) {
     if($chkVal && strlen(trim($chkTxt)) > 0) { return $chkVal; }
+    elseif (!$chkVal) { return$chkVal; }
     return NULL;
 }
 
