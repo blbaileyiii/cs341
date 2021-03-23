f6b6bebfc59fc49fb65e4a29de6f35317d80be45
iii <blbaileyiii.naa@gmail.com>
2021-03-23 14:55:27 -0700

    a

--- web/2021/models/reg-model.php
+++ web/2021/models/reg-model.php
@@ -11,16 +11,16 @@ function regParticipant($eventId, $participantName, $ward, $participantDOB, $par
         // The SQL statement
 
         $sql = 
-        "INSERT INTO hhstake.registrants (event_id, p_name, p_ward, p_dob, p_age, email, tele_one, tele_one_type, tele_two, tele_two_type, p_address, p_city, p_state, emer_name, emer_tele_one, emer_tele_one_type, emer_tele_two, emer_tele_two_type, diet, diet_txt, allergies, allergies_txt, medication, self_medicate, medication_txt, chronic, chronic_txt, serious, serious_txt, limitations_txt, considerations_txt, p_sig, p_sig_date, g_sig, g_sig_date, adult, contact, permission, responsibility, p_esig, g_esig)
-        VALUES ($eventId, $participantName, $ward, $participantDOB, $participantAge, $email, $primTel, $primTelType, $secTel, $secTelType, $participantAddress, $participantCity, $participantState, $emergencyContact, $emerPrimTel, $emerPrimTelType, $emerSecTel, $emerSecTelType, $specialDiet, $specialDietTxt, $allergies, $allergiesTxt, $medication, $selfMedicate, $medicationList, $chronicIllness, $chronicIllnessTxt, $serious, $seriousTxt, $limitations, $considerations, $participantSig, $participantSigDate, $guardianSig, $guardianSigDate, $adult, $contact, $permission, $responsibility, $participantESig, $guardianESig)
-        RETURNING id";
+        'INSERT INTO hhstake.registrants (event_id, p_name, p_ward, p_dob, p_age, email, tele_one, tele_one_type, tele_two, tele_two_type, p_address, p_city, p_state, emer_name, emer_tele_one, emer_tele_one_type, emer_tele_two, emer_tele_two_type, diet, diet_txt, allergies, allergies_txt, medication, self_medicate, medication_txt, chronic, chronic_txt, serious, serious_txt, limitations_txt, considerations_txt, p_sig, p_sig_date, g_sig, g_sig_date, adult, contact, permission, responsibility, p_esig, g_esig)
+        VALUES (:eventId, :participantName, :ward, :participantDOB, :participantAge, :email, :primTel, :primTelType, :secTel, :secTelType, :participantAddress, :participantCity, :participantState, :emergencyContact, :emerPrimTel, :emerPrimTelType, :emerSecTel, :emerSecTelType, :specialDiet, :specialDietTxt, :allergies, :allergiesTxt, :medication, :selfMedicate, :medicationList, :chronicIllness, :chronicIllnessTxt, :serious, :seriousTxt, :limitations, :considerations, :participantSig, :participantSigDate, :guardianSig, :guardianSigDate, :adult, :contact, :permission, :responsibility, :participantESig, :guardianESig)
+        RETURNING id';
 
-        //VALUES (:eventId, :participantName, :ward, :participantDOB, :participantAge, :email, :primTel, :primTelType, :secTel, :secTelType, :participantAddress, :participantCity, :participantState, :emergencyContact, :emerPrimTel, :emerPrimTelType, :emerSecTel, :emerSecTelType, :specialDiet, :specialDietTxt, :allergies, :allergiesTxt, :medication, :selfMedicate, :medicationList, :chronicIllness, :chronicIllnessTxt, :serious, :seriousTxt, :limitations, :considerations, :participantSig, :participantSigDate, :guardianSig, :guardianSigDate, :adult, :contact, :permission, :responsibility, :participantESig, :guardianESig)
-        //RETURNING id';
+        //VALUES ($eventId, $participantName, $ward, $participantDOB, $participantAge, $email, $primTel, $primTelType, $secTel, $secTelType, $participantAddress, $participantCity, $participantState, $emergencyContact, $emerPrimTel, $emerPrimTelType, $emerSecTel, $emerSecTelType, $specialDiet, $specialDietTxt, $allergies, $allergiesTxt, $medication, $selfMedicate, $medicationList, $chronicIllness, $chronicIllnessTxt, $serious, $seriousTxt, $limitations, $considerations, $participantSig, $participantSigDate, $guardianSig, $guardianSigDate, $adult, $contact, $permission, $responsibility, $participantESig, $guardianESig)
+        //RETURNING id";
         // Create the prepared statement using the phpmotors connection
         $stmt = $db->prepare($sql);
         // Build var array
-        /*
+        
         $sqlVarArray = array(
             ':eventId' => $eventId,
             ':participantName' => $participantName,
@@ -64,10 +64,10 @@ function regParticipant($eventId, $participantName, $ward, $participantDOB, $par
             ':participantESig' => $participantESig,
             ':guardianESig' => $guardianESig
         );
-        */
+        
         // Insert the data
-        //$stmt->execute($sqlVarArray);
-        $stmt->execute();
+        $stmt->execute($sqlVarArray);
+        //$stmt->execute();
         // Ask how many rows changed as a result of our insert
         $regResults = $stmt->fetchAll();
         if (count($regResults === 0)){
