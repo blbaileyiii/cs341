a3113a0274ec31ae4a0c301522335ae8b44cc788
iii <blbaileyiii.naa@gmail.com>
2021-03-23 13:35:13 -0700

    a

--- learnyounode/baby-steps.js
+++
@@ -1,6 +0,0 @@
-let myArgs = process.argv.slice(2);
-
-let sum = 0;
-myArgs.forEach(arg => sum += parseInt(arg));
-
-console.log(sum);
--- learnyounode/my-first-io.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const file = process.argv[2];
-const buffer = fs.readFileSync(file);
-let str = buffer.toString();
-let newLineArray = str.split("\n");
-
-console.log(newLineArray.length - 1);
