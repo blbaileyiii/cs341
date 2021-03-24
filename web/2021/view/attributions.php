c12cebb9416dd9014557a8c60939b43953f0885b
iii <blbaileyiii.naa@gmail.com>
2021-03-24 13:42:13 -0700

    a

--- web/2021/view/attributions.php
+++ web/2021/view/attributions.php
@@ -14,23 +14,23 @@
         <h1><?php echo $page; ?></h1>
         <div class="attributions">
             <figure>
-                <a href="https://unsplash.com/photos/Yizrl9N_eDA" target="_blank" rel="noreferrer"><img class="landscape" src="/2021/images/hike-w1280x853.webp" alt="Hiking"></a>
+                <a href="https://unsplash.com/photos/Yizrl9N_eDA" target="_blank" rel="noreferrer" title="Hiking"><img class="landscape" src="/2021/images/hike-w1280x853.webp" alt="Hiking"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@toomastartes" target="_blank" rel="noreferrer">Toomas Tartes</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="https://unsplash.com/photos/bQl2kRQyUE8" target="_blank" rel="noreferrer"><img class="landscape" src="/2021/images/equipment-w1280x853.webp" alt="Camping Equipment"></a>
+                <a href="https://unsplash.com/photos/bQl2kRQyUE8" target="_blank" rel="noreferrer" title="Camping Equipment"><img class="landscape" src="/2021/images/equipment-w1280x853.webp" alt="Camping Equipment"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@nineteen" target="_blank" rel="noreferrer">Andrew Ly</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="https://unsplash.com/photos/fDostElVhN8" target="_blank" rel="noreferrer"><img class="landscape" src="/2021/images/social-w1280x853.webp" alt="Social/Camp Fire"></a>
+                <a href="https://unsplash.com/photos/fDostElVhN8" target="_blank" rel="noreferrer" title="Social/Camp Fire"><img class="landscape" src="/2021/images/social-w1280x853.webp" alt="Social/Camp Fire"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@tegan" target="_blank" rel="noreferrer">Tegan Mierle</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="https://unsplash.com/photos/w55SpMmoPgE" target="_blank" rel="noreferrer"><img class="landscape" src="/2021/images/physical-w1280x853.webp" alt="Tug of War"></a>
+                <a href="https://unsplash.com/photos/w55SpMmoPgE" target="_blank" rel="noreferrer" title="Tug of War"><img class="landscape" src="/2021/images/physical-w1280x853.webp" alt="Tug of War"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@hagalnaud" target="_blank" rel="noreferrer">Anna Samoylova</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="https://unsplash.com/photos/gGbuETcoKjw" target="_blank" rel="noreferrer"><img class="landscape" src="/2021/images/spiritual-w1280x854.webp" alt="Scriptures/Outdoors"></a>
+                <a href="https://unsplash.com/photos/gGbuETcoKjw" target="_blank" rel="noreferrer" title="Scriptures/Outdoors"><img class="landscape" src="/2021/images/spiritual-w1280x854.webp" alt="Scriptures/Outdoors"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@benwhitephotography" target="_blank" rel="noreferrer">Ben White</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
@@ -38,12 +38,12 @@
                 <figcaption>Photo by <a href="https://www.pexels.com/@charlotte-may?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels" target="_blank" rel="noreferrer">Charlotte May</a> from <a href="https://www.pexels.com/" target="_blank" rel="noreferrer">Pexels</a> - <a href="https://www.pexels.com/license/" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
             <figure>
-                <a href="https://unsplash.com/photos/i4Fv22LzcQU" target="_blank" rel="noreferrer"><img class="portrait" src="/2021/images/growth-path-w1280x1707.webp" alt="Path Through Growing Trees"></a>
+                <a href="https://unsplash.com/photos/i4Fv22LzcQU" target="_blank" rel="noreferrer" title="Path Through Growing Trees"><img class="portrait" src="/2021/images/growth-path-w1280x1707.webp" alt="Path Through Growing Trees"></a>
                 <figcaption>Photo by <a href="https://unsplash.com/@jane_palash" target="_blank" rel="noreferrer">Jane Palash</a> on <a href="https://unsplash.com" target="_blank" rel="noreferrer">Unsplash</a> - <a href="https://unsplash.com/license" target="_blank" rel="noreferrer">License</a></figcaption>
             </figure>
 
             <figure>
-            <img class="portrait" src="/favicon.ico" alt="Backpack Icon">
+                <a href="https://www.flaticon.com/free-icon/backpack_3127370?term=backpack&page=1&position=26&page=1&position=26&related_id=3127370&origin=search" target="_blank" rel="noreferrer" title="backpack icon on flaticon.com"><img class="portrait" src="/favicon.ico" alt="Backpack Icon"></a>
                 <figcaption>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></figcaption>
             </figure>
             <!--
