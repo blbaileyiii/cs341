9bca92a7c3ea5868c936e3770ebf6798d1fd52cd
iii <blbaileyiii.naa@gmail.com>
2021-03-24 11:07:55 -0700

    a

--- web/2021/query/index.php
+++ web/2021/query/index.php
@@ -27,7 +27,10 @@ switch($action){
         $owned = filter_input(INPUT_POST, 'owned', FILTER_SANITIZE_STRING);
         $pur_price = filter_input(INPUT_POST, 'pur_price', FILTER_SANITIZE_NUMBER_FLOAT);
         
-        echo "{'success': 'true'}";
+        $JSON->success = true;
+
+        $myJSON = json_encode($JSON);
+        echo  $myJSON;
         // TODO VALIDATE THE 4 to make sure they are what they say they are, 
         // and then if they all exist push on...
         //postItemJSON($reg_id, $item_id, $owned, $pur_price);
