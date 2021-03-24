1a4ac661514cfb2576e1445eee3f16804fcfab1f
iii <blbaileyiii.naa@gmail.com>
2021-03-24 16:23:47 -0700

    a

--- web/2021/js/Participants.js
+++ web/2021/js/Participants.js
@@ -4,6 +4,7 @@ export default class Participants {
     constructor() {
         this.list = this.getParticipants();
         // Validate the list...loop through it and if the participant is gone, remove and update...
+        this.validateList(this);
     }
 
     getParticipants() {
@@ -12,6 +13,42 @@ export default class Participants {
             list = [];
         }
         return list;
+    }
+
+    validateList(master){
+        if (this.list.length > 0) {
+            this.list.forEach(participant => {
+                console.log(participant);
+                this.getParticipantById(master, participant.id)
+                //this.displayItemCheckList(participant);
+            });            
+        } else {
+            this.getEquipment(master, null)
+            //this.displayItemList();
+        }
+    }
+
+    getParticipantById(master, id) {
+        let url = "/2021/query/?action=getParticipant&reg_id=" + id;
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            //do stuff with data...
+            if (this.readyState == 4 && this.status == 200) {
+                let myDBRes = JSON.parse(this.responseText);
+                console.log(myDBRes);
+                master.convertEquipmentList(myDBRes, id);
+            } else if (this.readyState == 4 && this.status == 404) {
+                /*
+                let err404 = document.createElement("p");
+                err404.className = "err404";
+                err404.textContent = "404: JSON file not found. Try again; perhaps using a valid file name this time."
+                */
+            } else {
+                //console.log("failed");
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
     }
 
     
