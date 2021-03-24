6df5d4ba02182ab7cf9ebe791ecb353424e43b4e
iii <blbaileyiii.naa@gmail.com>
2021-03-24 07:30:11 -0700

    a

--- web/2021/models/query-model.php
+++ web/2021/models/query-model.php
@@ -17,13 +17,13 @@ function getEventsJSON($eventYear) {
 
         $stmt = $db->prepare($sql);
         $stmt->execute($sqlVarArray);
-        $eventSQL = $stmt->fetchAll();
-        $eventSQL = json_encode($eventSQL);
+        $returnSQL = $stmt->fetchAll();
+        $returnSQL = json_encode($returnSQL);
 
         // The next line closes the interaction with the database 
         $stmt->closeCursor();
 
-        return $eventSQL;
+        return $returnSQL;
 
     } catch(PDOException $ex) {
         echo $sql . "<br>" . $ex->getMessage();
@@ -41,17 +41,119 @@ function getEquipmentJSON() {
 
         $stmt = $db->prepare($sql);
         $stmt->execute();
-        $equipmentSQL = $stmt->fetchAll();
-        $equipmentSQL = json_encode($equipmentSQL);
+        $returnSQL = $stmt->fetchAll();
+        $returnSQL = json_encode($returnSQL);
 
         // The next line closes the interaction with the database 
         $stmt->closeCursor();
 
-        return $equipmentSQL;
+        return $returnSQL;
 
     } catch(PDOException $ex) {
         echo $sql . "<br>" . $ex->getMessage();
     }
 }
+
+function postItemJSON($reg_id, $item_id, $owned, $pur_price) {
+    try {
+        $db = hhConnect();
+
+        $sql = 
+        'INSERT INTO hhstake.inventory (reg_id, item_id, owned, pur_price)
+        VALUES (:reg_id, :item_id, :owned, :pur_price)
+        ON CONFLICT ON CONSTRAINT uq_reg_id_item_id
+        DO UPDATE
+        SET reg_id = :reg_id,
+            item_id = :item_id,
+            owned = :owned,
+            pur_price = :pur_price';
+        
+        $sqlVarArray = array(
+            ':reg_id' => $reg_id,
+            ':reg_id' => $item_id,
+            ':reg_id' => $owned,
+            ':reg_id' => $pur_price
+        );
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute($sqlVarArray);
+        $returnSQL = $stmt->fetchAll();
+        $returnSQL = json_encode($returnSQL);
+
+        // The next line closes the interaction with the database 
+        $stmt->closeCursor();
+
+        return $returnSQL;
+
+    } catch(PDOException $ex) {
+        echo $sql . "<br>" . $ex->getMessage();
+    }
+}
+
+/*
+INSERT INTO the_table (id, column_1, column_2) 
+VALUES (1, 'A', 'X'), (2, 'B', 'Y'), (3, 'C', 'Z')
+ON CONFLICT (id) DO UPDATE 
+  SET column_1 = excluded.column_1, 
+      column_2 = excluded.column_2;
+
+function getParticipantById($id){
+    try {
+        $db = hhConnect();
+
+        $sql = 
+        'SELECT id, p_name
+        FROM hhstake.registrants AS r
+        WHERE r.id = :id
+        ORDER BY r.id';
+
+        $sqlVarArray = array(
+            ':id' => $id
+        );
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute($sqlVarArray);
+        $returnSQL = $stmt->fetchAll();
+        $returnSQL = json_encode($returnSQL);
+
+        // The next line closes the interaction with the database 
+        $stmt->closeCursor();
+
+        return $returnSQL;
+
+    } catch(PDOException $ex) {
+        echo $sql . "<br>" . $ex->getMessage();
+    }
+}
+
+function getInventoryByPId($reg_id) {
+    try {
+        $db = hhConnect();
+
+        $sql = 
+        'SELECT *
+        FROM hhstake.invenotry AS i
+        WHERE i.reg_id = :reg_id
+        ORDER BY i.reg_id';
+
+        $sqlVarArray = array(
+            ':reg_id' => $reg_id
+        );
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute($sqlVarArray);
+        $returnSQL = $stmt->fetchAll();
+        $returnSQL = json_encode($returnSQL);
+
+        // The next line closes the interaction with the database 
+        $stmt->closeCursor();
+
+        return $returnSQL;
+
+    } catch(PDOException $ex) {
+        echo $sql . "<br>" . $ex->getMessage();
+    }
+}
+*/
 
 ?>
--- web/2021/query/index.php
+++ web/2021/query/index.php
@@ -22,6 +22,13 @@ switch($action){
     case 'getItems':
         break;
     case 'postItem':
+        $reg_id = filter_input(INPUT_POST, 'reg_id', FILTER_SANITIZE_NUMBER_INT);
+        $item_id = filter_input(INPUT_POST, 'item_id', FILTER_SANITIZE_NUMBER_INT);
+        $owned = filter_input(INPUT_POST, 'owned', FILTER_SANITIZE_STRING);
+        $pur_price = filter_input(INPUT_POST, 'pur_price', FILTER_SANITIZE_NUMBER_FLOAT);
+        // TODO VALIDATE THE 4 to make sure they are what they say they are, 
+        // and then if they all exist push on...
+        // postItemJSON($reg_id, $item_id, $owned, $pur_price);
         break;
     default:
         break;
