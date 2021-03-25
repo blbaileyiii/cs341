4aa746a9f8564c011f3c382c5fc1c9f4ca07bc3c
iii <blbaileyiii.naa@gmail.com>
2021-03-25 12:16:18 -0700

    a

--- web/2021/js/Participants.js
+++ web/2021/js/Participants.js
@@ -52,7 +52,7 @@ export default class Participants {
                     })
                     console.log(newList);
                     master.list = newList;
-                    //master.saveParticipants();
+                    master.saveParticipants();
                 }
                 //master.validationComplete();
             } else if (this.readyState == 4 && this.status == 404) {
