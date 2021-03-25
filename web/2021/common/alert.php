8165a46458d961ab622f9b5511328f803d3cf2f9
iii <blbaileyiii.naa@gmail.com>
2021-03-25 08:18:11 -0700

    a

--- web/2021/common/alert.php
+++ web/2021/common/alert.php
@@ -1,6 +1,6 @@
 <?php
     $alert = "Site Under Construction";
     if(isset($alert)){
-        echo "<div class='site-alert'><span>$alert</span><span id='close-alert'>✖</span></div><script src='/2021/js/alert.js'></script>";
+        echo "<div class='site-alert'><span>$alert</span><a id='close-alert'>✖</a></div><script src='/2021/js/alert.js'></script>";
     }
 ?>
