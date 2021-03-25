32bbf1e0e33fd625fecdfb6f5a0cc6ad7daba5db
iii <blbaileyiii.naa@gmail.com>
2021-03-25 08:16:26 -0700

    a

--- web/2021/js/alert.js
+++ web/2021/js/alert.js
@@ -1,12 +1,13 @@
-let myAlert = document.getElementById('close-alert');
+let myAlertX = document.getElementById('close-alert');
 
-myAlert.addEventListener('click', closeAlert, false);
+myAlertX.addEventListener('click', closeAlert, false);
 
 if(sessionStorage.getItem('alert') == 'hidden'){
     closeAlert();
 }
 
 function closeAlert()  {
+    let myAlert = document.querySelector('.site-alert');
     myAlert.classList.add('hidden');
     sessionStorage.setItem('alert', 'hidden');
 }
