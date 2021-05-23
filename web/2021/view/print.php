408c41e61da1bd64c8273d3bb2bd35a9ac55423d
iii <blbaileyiii.naa@gmail.com>
2021-05-23 12:50:37 -0700

    a

--- web/2021/view/print.php
+++ web/2021/view/print.php
@@ -11,7 +11,7 @@
         <section class="print-registrants">
             <?php echo $permissionSlipsHTML; ?>
         </section>
-        <?php var_dump($registrants); ?>
+        <?php // var_dump($registrants); ?>
     </main>
 </body>
 </html>
