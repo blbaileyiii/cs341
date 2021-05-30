a5a7640a1517867353aedc51b729e47d31597042
iii <blbaileyiii.naa@gmail.com>
2021-05-30 12:31:36 -0700

    a

--- web/2021/common/faq.php
+++ web/2021/common/faq.php
@@ -3,15 +3,59 @@
         <h2>F.A.Q.</h2>
         <ul>
             <li>
-                <button type="button" class="collapsible-btn"><span class="faq-q">If I have special needs is camp for me?</span><span class="expander">✚</span></button>
+                <button type="button" class="collapsible-btn"><span class="faq-q">How much does camp cost? How do I pay for camp?</span><span class="expander">✚</span></button>
                 <div class="collapsible-content">
-                <p>Yes. Camp is for you. The purpose of youth camps are to help youth interact with others and have fun. By nature, camp has been designed to be as inclusive as possible but certain considerations may require special attention and adaptation. If you have specific concerns, address them as early as possible with the camp director(s) of your specific event. Also, be sure to mention any considerations while registering. We want to help all youth have an enjoyable experience and participate.</p>
+                    <p>Camp is for all youth. Camp costs are typically handled at the ward level. Please discuss specifics and any concerns regarding the cost of camp or camp equipment with your Bishop.</p>
+                    <p>Additionally, wards typically organize one fundraiser per year to help offset the costs of camp. Members and parents may participate as they are able, but are under no obligation to do so.</p>
                 </div>
             </li>
             <li>
-                <button type="button" class="collapsible-btn"><span class="faq-q">Open Collapsible</span><span class="expander">✚</span></button>
+                <button type="button" class="collapsible-btn"><span class="faq-q">Can parents participate?</span><span class="expander">✚</span></button>
                 <div class="collapsible-content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Yes, in fact we encourage your participation. We need volunteers to provide adult supervision, teaching for classes, etc. Please contact the camp director to discuss ways you can help.</p>
+                </div>
+            </li>
+            <li>
+                <button type="button" class="collapsible-btn"><span class="faq-q">Where do I drop off my children for camp?</span><span class="expander">✚</span></button>
+                <div class="collapsible-content">
+                    <p>YWCAMP 2021: Please drop your children off at the Stake Center at 8 AM July 27th, 2021.</p>
+                    <p>YMCAMP 2021: Please drop your children off at the Stake Center at 9 AM July 27th, 2021.</p>
+                    <p>TREK 2021: TBD</p>
+                </div>
+            </li>
+            <li>
+                <button type="button" class="collapsible-btn"><span class="faq-q">How do I contact camp in case of an emergency?</span><span class="expander">✚</span></button>
+                <div class="collapsible-content">
+                    <p>Please contact your Bishop in the case of an emergency. They will have emergency contact information for each of the camps.</p>
+                </div>
+            </li>           
+            <li>
+                <button type="button" class="collapsible-btn"><span class="faq-q">What do sleeping arrangements look like?</span><span class="expander">✚</span></button>
+                <div class="collapsible-content">
+                    <p>On Church-sponsored overnight activities, leaders arrange sleeping accommodations so that male and female participants do not sleep in immediate proximity to each other. Male and female leaders must have separate sleeping facilities. Married couples may share the same quarters if appropriate facilities are available.</p>
+                    <p>On Church-sponsored overnight activities, a child or youth may not stay in the same tent or room as an adult unless (1) the adult is his or her parent or guardian or (2) there are at least two adults in the tent or room who are the same gender as the children or youth.</p>
+                    <p>If adult leaders and children or youth share other overnight facilities, such as a cabin, there must be at least two adults in the facility, and they must be the same gender as the child or youth.</p>
+                    <p>See <a href='https://abn.churchofjesuschrist.org/study/manual/general-handbook/20-activities?lang=eng#title_number2' title='Overnight Activities, General Handbook: Serving in The Church of Jesus Christ of Latter-day Saints'>Overnight Activities, General Handbook: Serving in The Church of Jesus Christ of Latter-day Saints (20.6.12 ¶:3-5)</a></p>
+                    <p>For additional information please discuss the topic with your Bishop or the camp director of your specific event.</p>
+                </div>
+            </li>
+            <li>
+                <button type="button" class="collapsible-btn"><span class="faq-q">What are the dress requirements?</span><span class="expander">✚</span></button>
+                <div class="collapsible-content">
+                    <p>We expect all youth to dress modestly and appropriately for camp.</p>
+                    <p>Equipment lists are provided for each camp, including what not to bring.</p>
+                    <p>However, the equipment lists cannot be expected to cover all items which would be appropriate or inappropriate. As such, consider the following when choosing what to wear:</p>
+                    <p>We have been continually counseled to dress modestly. When you are well groomed and modestly dressed, you invite the companionship of the Spirit.</p>
+                    <p>Never lower your standards of dress. Do not use camp as an excuse to be immodest.</p>
+                    <p>Immodest clothing is any clothing that is tight, sheer, or revealing in any other manner. Young women should avoid short shorts and short skirts, shirts that do not cover the stomach, and clothing that does not cover the shoulders or is low-cut in the front or the back. Young men should also maintain modesty in their appearance. Young men and young women should be neat and clean and avoid being extreme or inappropriately casual in clothing, hairstyle, and behavior. They should choose appropriately modest apparel when participating in sports.</p>
+                    <p>If you are not sure what is appropriate to wear, study the words of the prophets, pray for guidance, and ask your parents or leaders for help.</p>
+                    <p>See <a href='https://abn.churchofjesuschrist.org/study/manual/for-the-strength-of-youth/dress-and-appearance?lang=eng' title='Dress and Apprearance, Standards for Youth'>Dress and Apprearance, Standards for Youth</a></p>
+                </div>
+            </li>
+            <li>
+                <button type="button" class="collapsible-btn"><span class="faq-q">If I have special needs is camp for me?</span><span class="expander">✚</span></button>
+                <div class="collapsible-content">
+                    <p>Yes. Camp is for you. The purpose of youth camps are to help youth interact with others and have fun. By nature, camp has been designed to be as inclusive as possible but certain considerations may require special attention and adaptation. If you have specific concerns, address them as early as possible with the camp director(s) of your specific event. Also, be sure to mention any considerations while registering. We want to help all youth have an enjoyable experience and participate.</p>
                 </div>
             </li>
         </ul>
