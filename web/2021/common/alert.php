173e9c3434ac33311e1e51850131b30947b8af95
iii <blbaileyiii.naa@gmail.com>
2021-05-30 10:17:35 -0700

    a

--- web/2021/common/alert.php
+++ web/2021/common/alert.php
@@ -1,5 +1,5 @@
 <?php
-    $alert = "Registration Still Available During Site Construction.";
+    $alert = "Registration Available During Site Construction.";
     if(isset($alert)){
         echo "<div class='site-alert'><span>$alert</span><a href='' title='close alert' class='close-alert'>✖</a></div><script src='/2021/js/alert.js'></script>";
     }
