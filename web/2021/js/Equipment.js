3798cef6db7cb05c3cce1d799565cb2dd42978f4
iii <blbaileyiii.naa@gmail.com>
2021-07-15 08:33:39 -0700

    a

--- web/2021/js/Equipment.js
+++ web/2021/js/Equipment.js
@@ -108,7 +108,7 @@ export default class Equipment {
         // this.list = equipmentList;
         // console.log(equipmentList);
 
-        let equipmentList2 = {ymcamp: {}, ywcamp: {}, trek: {}};
+        let equipmentList2 = {ywcamp: {}, ymcamp: {}, trek: {}};
         // console.log(dbEquipment);
         dbEquipment.forEach(item => {
 
