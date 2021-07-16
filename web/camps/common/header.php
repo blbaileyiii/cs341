87c61b019414e89cf0fa034c2b46077ced236537
iii <blbaileyiii.naa@gmail.com>
2021-07-16 08:14:12 -0700

    a

--- web/camps/common/header.php
+++ web/camps/common/header.php
@@ -1,8 +1,8 @@
 <?php
     if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']){
-        $welcome = "<span><a href='/camps/accounts/?action=admin' title='HHSCamps - My Account'>Welcome {$_SESSION['clientData']['clientFirstname']}</a></span><span><a href='/camps/accounts/?action=Logout' title='HHSCamps - Logout'>Logout</a></span>";
+        $welcome = "<span>My Account: <a href='/camps/accounts/?action=admin' title='HHSCamps - My Account'>{$_SESSION['clientData']['clientAccount']}</a></span><span><a href='/camps/accounts/?action=Logout' title='HHSCamps - Logout'>Logout</a></span>";
     } else {        
-        $welcome = "<span><a href='/camps/accounts/?action=login' title='HHSCamps - Login'>My Account</a></span>";
+        $welcome = "<span><a href='/camps/accounts/?action=login' title='HHSCamps - Login'>Login</a></span>";
     }
 ?>
 <a href="/camps/" title="HHSCamps - Home"><img src="/camps/images/logo.png" alt="HHSCamps Logo"></a>
