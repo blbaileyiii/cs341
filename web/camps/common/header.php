bb62530f19df7ff14da2ee460e3ebb0c87096ed3
iii <blbaileyiii.naa@gmail.com>
2021-07-16 07:40:45 -0700

    a

--- web/camps/common/header.php
+++ web/camps/common/header.php
@@ -5,7 +5,5 @@
         $welcome = "<span><a href='/camps/accounts/?action=login' title='HHSCamps - Login'>My Account</a></span>";
     }
 ?>
-<header>
-    <a href="/camps/" title="HHSCamps - Home"><img src="/camps/images/logo.png" alt="HHSCamps Logo"></a>
-    <?php echo $welcome;?>
-</header>
+<a href="/camps/" title="HHSCamps - Home"><img src="/camps/images/logo.png" alt="HHSCamps Logo"></a>
+<?php echo $welcome;?>
