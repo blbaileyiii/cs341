a424f40c330eca2f2020a0efdd1b7dce2b900687
iii <blbaileyiii.naa@gmail.com>
2021-07-16 07:55:39 -0700

    a

--- web/camps/view/_template.php
+++ web/camps/view/_template.php
@@ -10,12 +10,14 @@
     <aside>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/camps/common/alert.php'; ?>
     </aside>
-    <header>
-        <?php require $_SERVER['DOCUMENT_ROOT'] . '/camps/common/header.php'; ?>
-    </header>
-    <nav>
-        <?php require $_SERVER['DOCUMENT_ROOT'] . '/camps/common/nav-expd.php'; ?>
-    </nav>    
+    <div class='sticky'>
+        <header>
+            <?php require $_SERVER['DOCUMENT_ROOT'] . '/camps/common/header.php'; ?>        
+        </header>
+        <nav>
+            <?php require $_SERVER['DOCUMENT_ROOT'] . '/camps/common/nav-expd.php'; ?>
+        </nav>
+    </div>    
     <main>    
         <h1>Content Title Here</h1>
     </main>
