5933cfa7c788b0189954ed0bacd90c1b8ab85c83
iii <blbaileyiii.naa@gmail.com>
2021-07-19 16:19:25 -0700

    a

--- web/2021/common/alert.php
+++ web/2021/common/alert.php
@@ -1,5 +1,5 @@
 <?php
-    $alert = "Registration Available During Site Construction.";
+    $alert = "TREK Registration Available. YWCAMP and YMCAMP Registration Locked - Contact Camp Leaders as needed. Site Construction Ongoing.";
     if(isset($alert)){
         echo "<div class='site-alert'><span>$alert</span><a href='' title='close alert' class='close-alert'>✖</a></div><script src='/2021/js/alert.js'></script>";
     }
