aca62730ca91b4c3bef1ce2326d34d621be41afd
iii <blbaileyiii.naa@gmail.com>
2021-08-01 14:12:25 -0700

    a

--- web/2021/view/esig.php
+++ web/2021/view/esig.php
@@ -16,12 +16,12 @@
     <main>    
         <h1><?php echo $page; ?></h1>
         <?php
-            // postESig(prepESig($_SERVER['DOCUMENT_ROOT'] . '/2021/images/gw2.jpg'))
-            $esig = getESig(1);
-            $esig = $esig[0]['img'];
-            echo $esig;
-            var_dump($esig);
-            echo "<img src='data:image/*;charset=utf8;base64,base64_encode($esig)'>";
+            postESig(prepESig($_SERVER['DOCUMENT_ROOT'] . '/2021/images/gw2.jpg'));
+            // $esig = getESig(1);
+            // $esig = $esig[0]['img'];
+            // echo $esig;
+            // var_dump($esig);
+            // echo "<img src='data:image/*;charset=utf8;base64,base64_encode($esig)'>";
             
         ?>
     </main>
