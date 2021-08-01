dadbe9c46f93345b74c261e7b654781bbac1422c
iii <blbaileyiii.naa@gmail.com>
2021-08-01 13:58:22 -0700

    a

--- web/2021/view/esig.php
+++ web/2021/view/esig.php
@@ -19,16 +19,8 @@
             // postESig(prepESig($_SERVER['DOCUMENT_ROOT'] . '/2021/images/gw2.jpg'))
             $esig = getESig(1);
             $esig = $esig[0]['img'];
-            // $esig = pg_unescape_bytea($esig);
-            var_dump($esig);
-
-            ob_start(); // Let's start output buffering.
-            fpassthru($esig); //This will normally output the image, but because of ob_start(), it won't.
-            $contents = ob_get_contents(); //Instead, output above is saved to $contents
-            ob_end_clean(); //End the output buffer.
-
-            $dataUri = "data:image/*;base64," . base64_encode($contents);
-            echo "<img src='$dataUri' />";
+            echo "<img src='data:image/*;charset=utf8;base64,base64_encode($esig)'";
+            
         ?>
     </main>
     <footer>
