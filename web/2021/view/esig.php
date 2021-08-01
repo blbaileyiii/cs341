d6866079785df7677331d55f0a4451a55194331d
iii <blbaileyiii.naa@gmail.com>
2021-08-01 12:47:25 -0700

    a

--- web/2021/view/esig.php
+++ web/2021/view/esig.php
@@ -16,7 +16,12 @@
     <main>    
         <h1><?php echo $page; ?></h1>
         <?php
-            $file_name = "woman.jpen";
+            $file_name = "/2021/images/gw2.jpg";
+            $img = fopen($file_name, 'r') or die("Cannot read image.");
+            $data = fread($img, filesize($file_name));
+            $es_data = pg_escape_bytea($data);
+            fclose($img);
+            postESig($es_data);
         ?>
     </main>
     <footer>
