5f683b0b9803a2924336b7c7eca8dd7e4c52d650
iii <blbaileyiii.naa@gmail.com>
2021-08-02 14:59:48 -0700

    a

--- web/2021/models/reg-model.php
+++ web/2021/models/reg-model.php
@@ -15,8 +15,8 @@ function regParticipant($eventId, $participantName, $ward, $participantDOB, $par
 
         // The SQL statement
         $sql = 
-        'INSERT INTO hhstake.registrants (event_id, p_name, p_ward, p_dob, p_age, email, tele_one, tele_one_type, tele_two, tele_two_type, p_address, p_city, p_state, emer_name, emer_tele_one, emer_tele_one_type, emer_tele_two, emer_tele_two_type, diet, diet_txt, allergies, allergies_txt, medication, self_medicate, medication_txt, chronic, chronic_txt, serious, serious_txt, limitations_txt, considerations_txt)
-        VALUES (:eventId, :participantName, :ward, :participantDOB, :participantAge, :email, :primTel, :primTelType, :secTel, :secTelType, :participantAddress, :participantCity, :participantState, :emergencyContact, :emerPrimTel, :emerPrimTelType, :emerSecTel, :emerSecTelType, :specialDiet, :specialDietTxt, :allergies, :allergiesTxt, :medication, :selfMedicate, :medicationList, :chronicIllness, :chronicIllnessTxt, :serious, :seriousTxt, :limitations, :considerations)
+        'INSERT INTO hhstake.registrants (event_id, p_name, p_ward, p_dob, p_age, email, tele_one, tele_one_type, tele_two, tele_two_type, p_address, p_city, p_state, emer_name, emer_tele_one, emer_tele_one_type, emer_tele_two, emer_tele_two_type, diet, diet_txt, allergies, allergies_txt, medication, self_medicate, medication_txt, chronic, chronic_txt, serious, serious_txt, limitations_txt, considerations_txt, adult, contact, permission, responsibility)
+        VALUES (:eventId, :participantName, :ward, :participantDOB, :participantAge, :email, :primTel, :primTelType, :secTel, :secTelType, :participantAddress, :participantCity, :participantState, :emergencyContact, :emerPrimTel, :emerPrimTelType, :emerSecTel, :emerSecTelType, :specialDiet, :specialDietTxt, :allergies, :allergiesTxt, :medication, :selfMedicate, :medicationList, :chronicIllness, :chronicIllnessTxt, :serious, :seriousTxt, :limitations, :considerations, :adult, :contact, :permission, :responsibility)
         RETURNING id';
 
         // 'INSERT INTO hhstake.registrants (event_id, p_name, p_ward, p_dob, p_age, email, tele_one, tele_one_type, tele_two, tele_two_type, p_address, p_city, p_state, emer_name, emer_tele_one, emer_tele_one_type, emer_tele_two, emer_tele_two_type, diet, diet_txt, allergies, allergies_txt, medication, self_medicate, medication_txt, chronic, chronic_txt, serious, serious_txt, limitations_txt, considerations_txt, adult, contact, permission, responsibility, p_esig, p_esig_date, g_esig, g_esig_date, userhost)
@@ -125,7 +125,11 @@ function regParticipant($eventId, $participantName, $ward, $participantDOB, $par
             ':serious' => $serious,
             ':seriousTxt' => $seriousTxt,
             ':limitations' => $limitations,
-            ':considerations' => $considerations
+            ':considerations' => $considerations,
+            ':adult' => $adult,
+            ':contact' => $contact,
+            ':permission' => $permission,
+            ':responsibility' => $responsibility
         );
         
         // Insert the data
