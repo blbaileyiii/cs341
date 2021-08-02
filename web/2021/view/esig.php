29c69a158f84c33220e364ab42e12f6dd114dd2f
iii <blbaileyiii.naa@gmail.com>
2021-08-02 07:32:37 -0700

    a

--- web/2021/view/esig.php
+++ web/2021/view/esig.php
@@ -56,6 +56,24 @@
                 var sigData = canvas.toDataURL("image/png");
                 $("#imgData").html('Thank you! Your signature was saved');
                 var ajax = new XMLHttpRequest();
+                ajax.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        // successful
+                        console.log(this.responseText);
+                    } else if (this.readyState == 4 && this.status == 404) {
+                        // unsuccessful
+                        console.log(this.responseText);
+                        /*
+                        let err404 = document.createElement("p");
+                        err404.className = "err404";
+                        err404.textContent = "404: JSON file not found. Try again; perhaps using a valid file name this time."
+                        */
+                    } else {
+                        // very unsuccessful
+                        console.log(this.responseText);
+                        // console.log("failed");
+                    }
+                }
                 ajax.open("POST", '/2021/sig/');
                 ajax.setRequestHeader('Content-Type', 'application/upload');
                 ajax.send(sigData);
