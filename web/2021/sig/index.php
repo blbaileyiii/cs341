646f0363f2d745e2d3478cfea4c48c4b70cdf730
iii <blbaileyiii.naa@gmail.com>
2021-08-02 07:20:17 -0700

    a

---
+++ web/2021/sig/index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Master Controller
+ */
+session_start();
+// Get the database connection file
+require_once $_SERVER['DOCUMENT_ROOT'] . '/2021/libraries/connections.php';
+// Get the registration model for use as needed
+require_once $_SERVER['DOCUMENT_ROOT'] . '/2021/models/query-model.php';
+// Get the fxs for valiation and file building
+require_once $_SERVER['DOCUMENT_ROOT'] . '/2021/libraries/fx.php';
+
+
+if (isset($GLOBALS["HTTP_RAW_POST_DATA"])) {
+    $imageData=$GLOBALS['HTTP_RAW_POST_DATA'];
+    postSig($imageData);
+}
+
+?>
