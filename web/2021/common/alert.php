29af93146bf8e5bfa8c7f910296cd5e0ef691cf2
iii <blbaileyiii.naa@gmail.com>
2021-08-02 15:48:32 -0700

    a

--- web/2021/common/alert.php
+++ web/2021/common/alert.php
@@ -1,5 +1,5 @@
 <?php
-    $alert = "TREK Registration UNavailable. Should be available by tonight 08/02/2021. Site Construction Ongoing.";
+    $alert = "TREK Registration UNavailable. Should be available by 08/04/2021. Site Construction Ongoing.";
     if(isset($alert)){
         echo "<div class='site-alert'><span>$alert</span><a href='' title='close alert' class='close-alert'>✖</a></div><script src='/2021/js/alert.js'></script>";
     }
