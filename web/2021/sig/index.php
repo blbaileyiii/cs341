243dc3c7849c96b90600c42f1060046d9ee3d663
iii <blbaileyiii.naa@gmail.com>
2021-08-02 08:23:51 -0700

    a

--- web/2021/sig/index.php
+++ web/2021/sig/index.php
@@ -11,25 +11,25 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/2021/models/query-model.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/2021/libraries/fx.php';
 
 
-// if (isset($_POST)) {
-//     $imageData=$_POST[0];
-//     echo postSig($imageData);
-// } else {
-//     echo 'FAILED';
-//     var_dump($_POST);
-// }
-
-$sigURL = filter_input(INPUT_POST, 'sig');
-var_dump(filter_input(INPUT_POST, 'sig'));
-$action = filter_input(INPUT_POST, 'action');
-var_dump(filter_input(INPUT_POST, 'action'));
-
-if(isset($sigURL)){
+if (isset($_POST)) {
+    $sigURL=$_POST[0];
     echo postSig($sigURL);
 } else {
     echo 'FAILED';
     var_dump($_POST);
 }
+
+// $sigURL = filter_input(INPUT_POST, 'sig');
+// var_dump(filter_input(INPUT_POST, 'sig'));
+// $action = filter_input(INPUT_POST, 'action');
+// var_dump(filter_input(INPUT_POST, 'action'));
+
+// if(isset($sigURL)){
+//     echo postSig($sigURL);
+// } else {
+//     echo 'FAILED';
+//     var_dump($_POST);
+// }
 
 
 
