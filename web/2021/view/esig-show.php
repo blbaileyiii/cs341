9bc9c499134add8145d9075c1929edad080fbee0
iii <blbaileyiii.naa@gmail.com>
2021-08-02 16:14:08 -0700

    a

--- web/2021/view/esig-show.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // postSig();
-    getSig(224);
-    // <img src="https://www.hhscamps.com/2021/?action=esig-show">
-    // <img src="https://www.hhscamps.com/2021/?action=getSig-show&id=224&type=p">
-    // <img src="https://www.hhscamps.com/2021/?action=getSig-show&id=224&type=g">
-?>
