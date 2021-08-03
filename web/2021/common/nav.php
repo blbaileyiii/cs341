b669ef3c63e87f6060b0fb0cec81f246059fc39a
iii <blbaileyiii.naa@gmail.com>
2021-08-03 10:56:13 -0700

    a

--- web/2021/common/nav.php
+++ web/2021/common/nav.php
@@ -1,5 +1,6 @@
-<div class="camp-yr-logo">
-    <a class='qr-hover qr-flex' href="/2021/" title="Hacienda Heights Stake Youth Camps Home"><span><input class="qr-trig" type="checkbox"><img class="qr-code qr-hidden" src='/2021/images/qr-code.png' alt='hhscamps qr code'></span><span class="logo-highlight-none logo-lg">Camp</span><span class="logo-highlight logo-sm">2021</span></a>
+<div class="camp-yr-logo qr-flex">
+    <span><input class="qr-trig" type="checkbox"><img class="qr-code qr-hidden" src='/2021/images/qr-code.png' alt='hhscamps qr code'></span>
+    <a href="/2021/" title="Hacienda Heights Stake Youth Camps Home"><span class="logo-highlight-none logo-lg">Camp</span><span class="logo-highlight logo-sm">2021</span></a>
 </div>
 <div class="menu">
     <span class="nav item ham"><a id="toggleMenu" href="javascript:void(0);">☰ Menu</a></span>
