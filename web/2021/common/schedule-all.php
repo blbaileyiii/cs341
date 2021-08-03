58e9760e321dca82310ca3612d4befb693e72338
iii <blbaileyiii.naa@gmail.com>
2021-08-03 11:43:52 -0700

    a

--- web/2021/common/schedule-all.php
+++ web/2021/common/schedule-all.php
@@ -2,8 +2,8 @@
     <div class="section-center">
         <h2>Camp Schedules</h2>
         <ul class="all-schedules">
-            <li class="camp-yr-logo"><a href="/2021/?action=ywcamp#schedule" title=""><span class="logo-highlight logo-lg">YWCamp</span><span class="logo-sm">2021</span></a></li>
-            <li class="camp-yr-logo"><a href="/2021/?action=ymcamp#schedule" title=""><span class="logo-highlight logo-lg">YMCamp</span><span class="logo-sm">2021</span></a></li>
+            <!-- <li class="camp-yr-logo"><a href="/2021/?action=ywcamp#schedule" title=""><span class="logo-highlight logo-lg">YWCamp</span><span class="logo-sm">2021</span></a></li> -->
+            <!-- <li class="camp-yr-logo"><a href="/2021/?action=ymcamp#schedule" title=""><span class="logo-highlight logo-lg">YMCamp</span><span class="logo-sm">2021</span></a></li> -->
             <li class="camp-yr-logo"><a href="/2021/?action=trek#schedule" title=""><span class="logo-highlight logo-lg">Trek</span><span class="logo-sm">2021</span></a></li>
         </ul>
     </div>
