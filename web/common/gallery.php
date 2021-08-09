1ecfd89a9e7c3f5233fde40da4d6629dddf13504
iii <blbaileyiii.naa@gmail.com>
2021-08-09 13:44:29 -0700

    a

--- web/common/gallery.php
+++ web/common/gallery.php
@@ -1,6 +1,12 @@
 <section id="gallery" class="gallery">
     <div class="section-center">
         <h2>Gallery</h2>
+        <div class='video'>
+        <video class='responsive' controls>
+            <source src="https://youtu.be/rLqL07Q_Fqk" type="video/mp4">
+            Your browser does not support HTML video.
+        </video>
+        </div>
         <div class='gallery-container'>
             <!-- Full-width images with number text -->
             <?php echo $galleryHTML; ?>
