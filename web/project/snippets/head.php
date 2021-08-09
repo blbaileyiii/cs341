285c506f8c47d06cfb22c1c4974b3f0e0fc978a0
iii <blbaileyiii.naa@gmail.com>
2021-08-09 08:04:33 -0700

    redesign to remove 2021 reference

--- web/project/snippets/head.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Echoes of Whimsy - <?php echo $page ?></title>
-<meta name="Echoes of Whimsy"
-    content="Echoes of Whimsy - <?php echo $page ?>">
-    <link rel="icon" href="/project/favicon.ico">
-    <link rel="stylesheet" href="/project/css/theme.css">
-    <link rel="stylesheet" href="/project/css/typography.css">
-    <link rel="stylesheet" href="/project/css/layout.css">
