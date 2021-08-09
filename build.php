f9cdb5d80b3f3b6dac547b2784c02917940e59d2
iii <blbaileyiii.naa@gmail.com>
2021-08-09 10:10:40 -0700

    a

---
+++ build.php
@@ -0,0 +1,7 @@
+$navList .= "<li class='nav item'><a href='/' title='Hacienda Heights Stake Youth Camps Home'>Home</a></li>";
+$navList .= "<li class='nav item'><a href='/?action=ywcamp' title='YW Camp'>YW Camp</a></li>";
+$navList .= "<li class='nav item'><a href='/?action=ymcamp' title='YM Camp'>YM Camp</a></li>";
+$navList .= "<li class='nav item'><a href='/?action=trek' title='Trek'>Trek</a></li>";
+$navList .= "<li class='nav item'><a href='/equipment' title='Equipment'>Equipment</a></li>";
+$navList .= "<li class='nav item'><a href='https://www.churchofjesuschrist.org/callings/church-safety-and-health/protecting-children-and-youth?lang=eng' title='Leadership Training' target='_blank' rel='noreferrer'>Leadership Training</a></li>";
+$navList .= "<li class='nav item'><a href='/registration' title='Register'>Register</a></li>";
