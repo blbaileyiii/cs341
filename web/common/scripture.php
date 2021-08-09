6f18ae39354f820aa6e96411b0e1feb8b10bb80d
iii <blbaileyiii.naa@gmail.com>
2021-08-09 12:05:18 -0700

    a

--- web/common/scripture.php
+++ web/common/scripture.php
@@ -1,5 +1,5 @@
 <section class="parallax scripture">
-    <div class="scripture" class="filter">
+    <div id="scripture" class="filter">
         <!-- 2021 <p><a href="https://www.churchofjesuschrist.org/study/scriptures/nt/luke/2.52?lang=eng&clang=eng#p52" title="Luke 2:52" target="_blank" rel="noreferrer">Luke 2:52</a> "And Jesus increased in wisdom and stature, and in favour with God and man."</p> -->
         <p><a href="https://www.churchofjesuschrist.org/study/scriptures/dc-testament/dc/64.33-34?lang=eng#32" title="Doctrine and Covenants 64:33-34" target="_blank" rel="noreferrer">Doctrine and Covenants 64:33-34</a> “Wherefore, be not weary in well-doing, for ye are laying the foundation of a great work. And out of small things proceedeth that which is great. Behold, the Lord requireth the heart and a willing mind”</p>
     </div>
