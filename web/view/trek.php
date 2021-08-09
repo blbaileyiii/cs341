efcb10c764064a8562774d419e0181cd13173d4b
iii <blbaileyiii.naa@gmail.com>
2021-08-09 12:17:38 -0700

    a

--- web/view/trek.php
+++ web/view/trek.php
@@ -4,8 +4,6 @@
 <head>
     <?php $page = "Template" ?>
     <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/head.php'; ?>
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
 </head>
 
 <body>
@@ -21,30 +19,6 @@
         <div class="schedule"></div>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/pillars.php'; ?>
         <div class="gallery">
-            <div class="owl-carousel owl-theme owl-loaded">
-                <div class="owl-stage-outer">
-                    <div class="owl-stage">
-                        <div class="owl-item">Does</div>
-                        <div class="owl-item">.It</div>
-                        <div class="owl-item">Work</div>
-                        <div class="owl-item">Or</div>
-                        <div class="owl-item">Not</div>
-                        <div class="owl-item">...</div>
-                    </div>
-                </div>
-                <div class="owl-nav">
-                    <div class="owl-prev">prev</div>
-                    <div class="owl-next">next</div>
-                </div>
-                <div class="owl-dots">
-                    <div class="owl-dot active"><span></span></div>
-                    <div class="owl-dot"><span></span></div>
-                    <div class="owl-dot"><span></span></div>
-                    <div class="owl-dot"><span></span></div>
-                    <div class="owl-dot"><span></span></div>
-                    <div class="owl-dot"><span></span></div>
-                </div>
-            </div>
         </div>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/faq.php'; ?>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/scripture.php'; ?>
@@ -55,12 +29,5 @@
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
     </footer>
     <script src="js/countdown.js"></script>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            $(".owl-carousel").owlCarousel();
-        });
-    </script>
 </body>
 </html>
