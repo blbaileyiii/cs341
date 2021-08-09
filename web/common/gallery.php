cac83d7ec2a2aa7b59e57eca50dfc21542f233af
iii <blbaileyiii.naa@gmail.com>
2021-08-09 14:42:58 -0700

    a

--- web/common/gallery.php
+++ web/common/gallery.php
@@ -2,16 +2,18 @@
     <div class="section-center">
         <h2>Gallery</h2>
         <div class='video'>
-            <!-- TREK Video (Pres. Rigby) -->
+            <!-- TREK Video (Pres. Roza) -->
             <iframe class='responsive-iframe' src="https://www.youtube.com/embed/rLqL07Q_Fqk"></iframe>
         </div>
         <div class='gallery-container'>
+            <div class='gallery-img-container'>
             <!-- Full-width images with number text -->
-            <?php echo $galleryHTML; ?>
+                <?php echo $galleryHTML; ?>
 
-            <!-- Next and previous buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                <!-- Next and previous buttons -->
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
 
             <!-- Image text -->
             <div class="caption-container hidden">
