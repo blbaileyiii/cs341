01d4ecb15d6734ffb612e3f4b0f79aa6affb9b3a
iii <blbaileyiii.naa@gmail.com>
2021-08-09 10:51:05 -0700

    a

--- web/common/schedule-all.php
+++ web/common/schedule-all.php
@@ -2,9 +2,9 @@
     <div class="section-center">
         <h2>Camp Schedules</h2>
         <ul class="all-schedules">
-            <!-- <li class="camp-yr-logo"><a href="/?action=ywcamp#schedule" title=""><span class="logo-highlight logo-lg">YWCamp</span><span class="logo-sm">2021</span></a></li> -->
-            <!-- <li class="camp-yr-logo"><a href="/?action=ymcamp#schedule" title=""><span class="logo-highlight logo-lg">YMCamp</span><span class="logo-sm">2021</span></a></li> -->
-            <li class="camp-yr-logo"><a href="/?action=trek#schedule" title=""><span class="logo-highlight logo-lg">Trek</span><span class="logo-sm">2021</span></a></li>
+            <!-- <li class="camp-yr-logo"><a href="/?action=ywcamp#schedule" title="ywcamp schedule"><span class="logo-highlight logo-lg">YWCamp</span><span class="logo-sm">2021</span></a></li> -->
+            <!-- <li class="camp-yr-logo"><a href="/?action=ymcamp#schedule" title="ymcamp schedule"><span class="logo-highlight logo-lg">YMCamp</span><span class="logo-sm">2021</span></a></li> -->
+            <li class="camp-yr-logo"><a href="/?action=getCamp&camp=trek#schedule" title="trek schedule"><span class="logo-highlight logo-lg">Trek</span><span class="logo-sm">2021</span></a></li>
         </ul>
     </div>
 </section>
