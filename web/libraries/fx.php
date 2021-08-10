7d7f11fef11ad0bac571ff1a6ba5562a8905cfb8
iii <blbaileyiii.naa@gmail.com>
2021-08-09 18:59:41 -0700

    a

--- web/libraries/fx.php
+++ web/libraries/fx.php
@@ -243,8 +243,8 @@ function buildRegistrantsHTML($registrants) {
         $registrantsHTML .= "<td>{$registrants[$x]['emer_name']}</td>";
         $registrantsHTML .= "<td>{$registrants[$x]['emer_tele_one']}</td>";
         $registrantsHTML .= "<td>{$registrants[$x]['emer_tele_one_type']}</td>";
-        $registrantsHTML .= "<td><button type='button' data-pid='{$registrants[$x]['id']}>Make Leader</button></td>";
-        $registrantsHTML .= "<td><button type='button' data-pid='{$registrants[$x]['id']}>Inactivate</button></td>";
+        $registrantsHTML .= "<td><button type='button' data-pid='{$registrants[$x]['id']}'>Make Leader</button></td>";
+        $registrantsHTML .= "<td><button type='button' data-pid='{$registrants[$x]['id']}'>Inactivate</button></td>";
         $registrantsHTML .= "</tr>";
     }
 
