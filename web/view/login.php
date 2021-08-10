2ae60b1b82a471bb5301fea96b460c75f0eee3a7
iii <blbaileyiii.naa@gmail.com>
2021-08-10 07:10:58 -0700

    a

--- web/view/login.php
+++ web/view/login.php
@@ -13,32 +13,34 @@
     <header>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/nav.php'; ?>
     </header>    
-    <main>    
-        <h1><?php echo ucwords($page); ?></h1>
-        <?php
-            if (isset($_SESSION['message'])) { 
-                    echo $_SESSION['message']; 
-                    unset($_SESSION['message']);
-            }
-        ?>
-        <form class="row gap" action="/account/" method="post">
-            <div class="fields">
-                <label for="account"><span>Account Name (Email Address)</span><span class="field-tip">Required</span></label>
-                <input id="account" name="account" type="text" autocomplete="username" required>
-            </div>
-            <div class="fields">
-                <label for="password"><span>Password</span><span class="field-tip">Required</span></label>
-                <input id="password" name="password" type="password" autocomplete="current-password" required>                    
-            </div>
-            <div class="non-fields">
-                <button id="action" name="action" value="Login" type="submit">Login</button>
-            </div>
-            <div class="non-fields">
-                <div class="button">
-                    <a href="?action=createAccount" title="HHSCamps - Create Account">Create Account</a>
-                </div>                    
-            </div>
-        </form>
+    <main>
+        <section>
+            <h1><?php echo ucwords($page); ?></h1>
+            <?php
+                if (isset($_SESSION['message'])) { 
+                        echo $_SESSION['message']; 
+                        unset($_SESSION['message']);
+                }
+            ?>
+            <form class="row gap" action="/account/" method="post">
+                <div class="fields">
+                    <label for="account"><span>Account Name (Email Address)</span><span class="field-tip">Required</span></label>
+                    <input id="account" name="account" type="text" autocomplete="username" required>
+                </div>
+                <div class="fields">
+                    <label for="password"><span>Password</span><span class="field-tip">Required</span></label>
+                    <input id="password" name="password" type="password" autocomplete="current-password" required>                    
+                </div>
+                <div class="non-fields">
+                    <button id="action" name="action" value="Login" type="submit">Login</button>
+                </div>
+                <div class="non-fields">
+                    <div class="button">
+                        <a href="?action=createAccount" title="HHSCamps - Create Account">Create Account</a>
+                    </div>                    
+                </div>
+            </form>
+        </section>
     </main>
     <footer>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
